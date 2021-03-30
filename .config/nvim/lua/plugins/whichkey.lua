17709c614da93fa31817f7eba63a8c0ef5c5109d
Niels Mentink <niels.mentink@gmail.com>
2021-03-31 01:00:44 +0200

    Added proper whichkey binds

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -12,9 +12,45 @@ vim.g.which_key_timeout = 100
 vim.g.which_key_use_floating_win = 0
 vim.g.which_key_max_size = 0
 
-keymap.w = {':w<CR>', 'save file'}
-keymap.q = {':BufferPrevious<CR>', 'prev buffer'}
-keymap.e = {':BufferNext<CR>', 'next buffer'}
-keymap.d = {':BufferClose<CR>', 'close buffer'}
+-- Buffers
+keymap.b = {
+    name = '﬘ Buffer',
+    w = {':w<CR>', ' Save'},
+    p = {':BufferPrevious<CR>', 'ﭢ Previous'},
+    n = {':BufferNext<CR>', 'ﭠ Next'},
+    d = {':BufferClose<CR>', ' Close'},
+}
+
+-- Telescope / searching
+keymap.f = {
+    name = ' Find',
+    f = {':Telescope find_files<CR>', ' File'},
+    b = {':Telescope buffers<CR>', '﬘ Buffer'},
+    t = {':Telescope grep_string<CR>', '縷 Text'},
+    s = {':Telescope live_grep<CR>', '﬍ String'},
+}
+
+-- Sidebar
+keymap.n = {
+    name = 'פּ Sidebar',
+    n = {':NvimTreeFindFile<CR>', ' Find current'},
+    t = {':NvimTreeToggle<CR>', ' Toggle'},
+}
+
+-- Config
+keymap.o = {
+    name = '煉 Config',
+    r = {':luafile ~/.config/nvim/init.lua<CR>', '勒 Reload'},
+    o = {':e ~/.config/nvim/init.lua<CR>', ' Open'},
+}
+
+-- Window commands
+keymap.w = {
+    name = '类 Window',
+    d = {':q<CR>', ' Close'},
+    q = {':qa<CR>', '﫧 Close All'},
+    v = {':vsp<CR>', ' Split Vertical'},
+    h = {':sp<CR>', '漢 Split Horizontal'},
+}
 
 wk.register_keymap('leader', keymap)
