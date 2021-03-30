df9d19e9cc8ffc4158810383b9433e2bab222c70
Niels Mentink <niels.mentink@gmail.com>
2021-03-31 01:00:36 +0200

    Updated old mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,9 +1,3 @@
---[[
---
--- https://github.com/junegunn/fzf/issues/453
---
-]]--
-
 -- Clear spacebar keybinds and set mapleader
 vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {noremap = true, silent = true})
 vim.g.mapleader = ' '
@@ -27,16 +21,6 @@ vim.api.nvim_set_keymap('n', '\\', ':set hlsearch!<CR>', {noremap = true, silent
 -- Shortcut to enter command
 vim.api.nvim_set_keymap('n', ';', ':', {noremap = true})
 vim.api.nvim_set_keymap('v', ';', ':', {noremap = true})
-
--- Nvim tree mappings
-vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<cr>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<leader>N', ':NvimTreeClose<cr>', {noremap = true})
-
--- Open Telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<cr>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<cr>', {noremap = true, silent = true})
-
-vim.api.nvim_set_keymap('n', '<leader>or', ':luafile ~/.config/nvim/init.lua<cr>', {noremap = true, silent = true})
 
 -- better window movement
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
