58c5e79c89285bfc0eef4183d59cd668d72abce6
Niels Mentink <niels@ment.ink>
2021-12-23 10:45:04 +0100

    ✨ Added wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+
+return {
+	font = wezterm.font_with_fallback({ "PragmataPro Mono Liga", "FiraCode Nerd Font" }),
+	font_size = 18,
+	color_scheme = "tokyonight",
+}
