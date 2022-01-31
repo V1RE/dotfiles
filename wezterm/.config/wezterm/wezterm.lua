1a2a1f8ef6232a42e524251ca09f43c4988a2b66
Niels Mentink <niels@ment.ink>
2022-02-01 00:38:03 +0100

    💄 Switched to onedarker colorscheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,12 @@ local wezterm = require("wezterm")
 return {
 	font = wezterm.font_with_fallback({ "PragmataPro Mono Liga", "FiraCode Nerd Font" }),
 	font_size = 18,
-	color_scheme = "tokyonight",
+	color_scheme = "onedarker",
 	hide_tab_bar_if_only_one_tab = true,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
 }
