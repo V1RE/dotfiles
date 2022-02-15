d260dab3ef00a88c062345a2b65166e2838b5a2b
Niels Mentink <niels@ment.ink>
2022-02-15 08:58:57 +0100

    ✨ Started working on new nvim config

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+require("options")
+require("keymaps")
+require("plugins")
+require("colorscheme")
+require("autocommands")
---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,47 @@
+local options = {
+	autoread = true,
+	backup = false,
+	clipboard = "unnamedplus",
+	cmdheight = 2,
+	completeopt = { "menuone", "noselect" },
+	conceallevel = 0,
+	cursorline = true,
+	expandtab = true,
+	fileencoding = "utf-8",
+	guifont = "PragmataPro Mono Liga:h16",
+	hlsearch = true,
+	ignorecase = true,
+	mouse = "a",
+	number = true,
+	numberwidth = 4,
+	pumheight = 10,
+	relativenumber = true,
+	scrolloff = 8,
+	shiftwidth = 2,
+	showmode = false,
+	showtabline = 2,
+	sidescrolloff = 8,
+	signcolumn = "yes",
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	tabstop = 2,
+	termguicolors = true,
+	timeoutlen = 100,
+	undofile = true,
+	updatetime = 300,
+	wrap = false,
+	writebackup = false,
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+vim.opt.shortmess:append("c")
+vim.opt.whichwrap:append("<,>,[,],h,l")
+
+vim.cmd([[set iskeyword+=-]])
+vim.cmd([[set formatoptions-=cro]])
