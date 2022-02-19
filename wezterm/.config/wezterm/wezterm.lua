e10b6dfac2df249f8f38ae2e3bc8a62371d9d3d6
Niels Mentink <niels@ment.ink>
2022-02-19 15:01:23 +0100

    💄 Updated wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,36 +1,31 @@
 local wezterm = require("wezterm")
 
+-- A helper function for my fallback fonts
+local function font_with_fallback(name, params)
+    local names = {
+    name,
+    "PragmataPro Mono Liga",
+    "FiraCode Nerd Font",
+    "codicon",
+    "nonicons",
+    "Noto Color Emoji",
+    "JetBrains Mono",
+    }
+    return wezterm.font_with_fallback(names, params)
+end
+
 return {
-	font = wezterm.font_with_fallback({ "PragmataPro Mono Liga", "FiraCode Nerd Font" }),
-	font_size = 18,
-	color_scheme = "onedarker",
-	hide_tab_bar_if_only_one_tab = true,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	keys = {
-		{
-			key = "[",
-			mods = "CMD",
-			action = {
-				Multiple = {
-					{ SendKey = { key = "b", mods = "CTRL" } },
-					{ SendKey = { key = "h", mods = "CTRL" } },
-				},
-			},
-		},
-		{
-			key = "]",
-			mods = "CMD",
-			action = {
-				Multiple = {
-					{ SendKey = { key = "b", mods = "CTRL" } },
-					{ SendKey = { key = "l", mods = "CTRL" } },
-				},
-			},
-		},
-	},
+  font = font_with_fallback("PragmataPro Mono Liga"),
+  font_size = 18,
+  color_scheme = "catppuccin",
+  colors = require("colors.catppuccin"),
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  keys = require("keys"),
+  window_decorations = "RESIZE",
 }
