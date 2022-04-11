1525cf9e2d63f6afb0b681eee96cbbb72a1ab80f
Niels Mentink <niels@ment.ink>
2022-04-11 22:32:47 +0200

    ✨ Added TabNine

--- nvim/.config/nvim/lua/config/cmp.lua
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -64,8 +64,9 @@ cmp.setup({
     }),
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+    --[[ ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expandable() then
@@ -80,7 +81,7 @@ cmp.setup({
     end, {
       "i",
       "s",
-    }),
+    }), ]]
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
@@ -102,7 +103,10 @@ cmp.setup({
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
+        nvim_lua = "[Lua]",
         luasnip = "[Snippet]",
+        cmp_tabnine = "[TabNine]",
+        emoji = "[Emoji]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
@@ -111,9 +115,12 @@ cmp.setup({
   },
   sources = {
     { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
+    { name = "nvim_lua" },
     { name = "path" },
+    { name = "luasnip" },
+    { name = "cmp_tabnine" },
+    { name = "buffer" },
+    { name = "emoji" },
   },
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
