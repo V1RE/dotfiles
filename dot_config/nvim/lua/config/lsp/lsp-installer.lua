c187df79d8af5c5147b7d99f93aa053bd7f5df8c
Niels Mentink <niels@ment.ink>
2022-06-01 14:00:39 +0200

    Update dot_config/nvim/lua/config/lsp/handlers.lua Update dot_config/nvim/lua/config/lsp/lsp-installer.lua Update dot_config/nvim/lua/config/lsp/null-ls.lua

--- dot_config/nvim/lua/config/lsp/lsp-installer.lua
+++ dot_config/nvim/lua/config/lsp/lsp-installer.lua
@@ -1,34 +1,27 @@
 local lspinstaller = require("nvim-lsp-installer")
 local lspconfig = require("lspconfig")
-local schemastore = require("schemastore")
 
 lspinstaller.setup({
-	ui = {
-		icons = {
-			server_installed = "",
-			server_pending = "",
-			server_uninstalled = "",
-		},
-	},
+  ui = {
+    icons = {
+      server_installed = "",
+      server_pending = "",
+      server_uninstalled = "",
+    },
+  },
 })
 
 local capabilities = require("config.lsp.handlers").capabilities
 local on_attach = require("config.lsp.handlers").on_attach
 
 for _, server in ipairs(lspinstaller.get_installed_servers()) do
-	lspconfig[server.name].setup({
-		settings = {
-			json = {
-				schemas = schemastore.json.schemas(),
-			},
-		},
+  lspconfig[server.name].setup({
+    on_attach = on_attach,
 
-		on_attach = on_attach,
+    flags = {
+      debounce_text_changes = 150,
+    },
 
-		flags = {
-			debounce_text_changes = 150,
-		},
-
-		capabilities = capabilities,
-	})
+    capabilities = capabilities,
+  })
 end
