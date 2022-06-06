b48b0c51b781ed4976f398ae6332c23f4dfedcf9
Niels Mentink <niels@ment.ink>
2022-06-06 20:26:02 +0200

    Update dot_config/nvim/lua/plugins.lua Update dot_config/nvim/lua/profiler.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -15,8 +15,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 -- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
+local packer_ok, packer = pcall(require, "packer")
+if not packer_ok then
 	return
 end
 
--- dot_config/nvim/lua/profiler.lua
+++ dot_config/nvim/lua/profiler.lua
@@ -1,3 +1,6 @@
-local impatient = require("impatient")
+local impatient_ok, impatient = pcall(require, "impatient")
+if not impatient_ok then
+	return
+end
 
 impatient.enable_profile()
