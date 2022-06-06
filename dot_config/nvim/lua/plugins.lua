1e066036dad815d574511f41f095a308ba6d4761
Niels Mentink <niels@ment.ink>
2022-06-06 19:36:59 +0200

    Add dot_config/nvim/lua/config/legendary-nvim.lua Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -14,14 +14,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
@@ -227,6 +219,11 @@ return packer.startup(function(use)
 	use({
 		"lewis6991/gitsigns.nvim",
 		config = require("config.gitsigns"),
+	})
+
+	use({
+		"mrjones2014/legendary.nvim",
+		config = require("config.legendary-nvim"),
 	})
 
 	use({
