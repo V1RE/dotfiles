a66a6f40e54c14ae4954148400fd1b25fab31cb7
Niels Mentink <niels@ment.ink>
2022-06-09 10:22:32 +0200

    Update dot_config/nvim/lua/config/lsp/null-ls.lua

--- dot_config/nvim/lua/config/lsp/null-ls.lua
+++ dot_config/nvim/lua/config/lsp/null-ls.lua
@@ -1,10 +1,12 @@
 local null_ls = require("null-ls")
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client) return client.name ~= "tsserver" end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name ~= "tsserver"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -17,31 +19,34 @@ local linters = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    formatting.stylelint,
-    formatting.eslint_d,
-    formatting.prettierd,
-    formatting.stylua,
-    formatting.rubocop,
-    formatting.google_java_format,
-    formatting.phpcsfixer,
-    linters.stylelint,
-    linters.luacheck,
-    linters.rubocop,
-    linters.eslint_d,
-    code_actions.eslint_d,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
-  end,
+	debug = false,
+	sources = {
+		formatting.stylelint,
+		formatting.eslint_d,
+		formatting.prettierd,
+		formatting.stylua,
+		formatting.rubocop,
+		formatting.google_java_format,
+		formatting.phpcsfixer,
+		formatting.shfmt,
+		linters.stylelint,
+		linters.luacheck,
+		linters.rubocop,
+		linters.eslint_d,
+		linters.shellcheck,
+		code_actions.eslint_d,
+		code_actions.shellcheck,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
