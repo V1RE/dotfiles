844fb7eff31ba3b9d4c31afec76eadb516ba1667
Niels Mentink <niels@ment.ink>
2022-06-12 19:18:17 +0200

    Update dot_config/nvim/lua/config/telescope.lua Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -92,15 +92,8 @@ telescope.setup({
 		},
 	},
 	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
 		find_files = {
-			find_command = { "fd", "--type=file", "--hidden", "--exclude=.git" },
+			find_command = { "fd", "--type=file", "--hidden", "--exclude=.git", "--strip-cwd-prefix" },
 		},
 	},
 
@@ -111,11 +104,6 @@ telescope.setup({
 			override_file_sorter = true, -- override the file sorter
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 		},
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
 	},
 })
 
