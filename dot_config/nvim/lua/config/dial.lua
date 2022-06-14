e1de68e11193e543dac085104f250688b27eb6f0
Niels Mentink <niels@ment.ink>
2022-06-15 01:39:50 +0200

    Update dot_config/nvim/lua/config/dial.lua Update dot_config/nvim/lua/config/toggleterm.lua Update dot_config/nvim/lua/config/whichkey.lua

--- dot_config/nvim/lua/config/dial.lua
+++ dot_config/nvim/lua/config/dial.lua
@@ -1,11 +1,28 @@
-local dial_ok, dial = pcall(require, "dial.map")
+local dial_ok, dial = pcall(require, "dial.config")
 if not dial_ok then
-	return
+  return
 end
 
-vim.keymap.set("n", "<C-a>", dial.inc_normal)
-vim.keymap.set("n", "<C-x>", dial.dec_normal)
-vim.keymap.set("v", "<C-a>", dial.inc_visual)
-vim.keymap.set("v", "<C-x>", dial.dec_visual)
-vim.keymap.set("v", "g<C-a>", dial.inc_gvisual)
-vim.keymap.set("v", "g<C-x>", dial.dec_gvisual)
+local augend = require("dial.augend")
+
+dial.augends:register_group({
+  default = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.integer.alias.binary,
+    augend.date.alias["%Y/%m/%d"],
+    augend.date.alias["%Y-%m-%d"],
+    augend.date.alias["%H:%M:%S"],
+    augend.hexcolor.new({ case = "lower" }),
+    augend.semver.alias.semver,
+  },
+})
+
+local map = require("dial.map")
+
+vim.keymap.set("n", "<C-a>", map.inc_normal)
+vim.keymap.set("n", "<C-x>", map.dec_normal)
+vim.keymap.set("v", "<C-a>", map.inc_visual)
+vim.keymap.set("v", "<C-x>", map.dec_visual)
+vim.keymap.set("v", "g<C-a>", map.inc_gvisual)
+vim.keymap.set("v", "g<C-x>", map.dec_gvisual)
