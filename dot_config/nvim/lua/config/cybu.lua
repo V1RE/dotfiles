36b2b8991d14952e5462d77abf97e27ef16c6e98
Niels Mentink <niels@ment.ink>
2022-06-15 01:55:18 +0200

    Update dot_config/nvim/lua/config/cybu.lua

--- dot_config/nvim/lua/config/cybu.lua
+++ dot_config/nvim/lua/config/cybu.lua
@@ -1,20 +1,24 @@
 local cybu_ok, cybu = pcall(require, "cybu")
 if not cybu_ok then
-	return
+  return
 end
 
 cybu.setup({
-	position = {
-		relative_to = "editor",
-	},
-	style = {
-		path = "tail",
-	},
-	exclude = {
-		"neo-tree",
-		"fugitive",
-		"qf",
-		"NvimTree",
-		"alpha",
-	},
+  position = {
+    relative_to = "editor",
+    anchor = "center",
+    max_win_height = 7,
+  },
+  style = {
+    path = "tail",
+    padding = 2,
+    hide_buffer_id = true,
+  },
+  exclude = {
+    "neo-tree",
+    "fugitive",
+    "qf",
+    "NvimTree",
+    "alpha",
+  },
 })
