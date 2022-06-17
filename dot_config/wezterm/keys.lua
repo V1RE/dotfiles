497e8231847d89a0ded603e0ce36152028e58f96
Niels Mentink <niels@ment.ink>
2022-06-17 03:51:17 +0200

    Update dot_config/wezterm/keys.lua

--- dot_config/wezterm/keys.lua
+++ dot_config/wezterm/keys.lua
@@ -1,22 +1,18 @@
 return {
-  {
-    key = "[",
-    mods = "CMD",
-    action = {
-      Multiple = {
-        { SendKey = { key = "b", mods = "CTRL" } },
-        { SendKey = { key = "h", mods = "CTRL" } },
-      },
-    },
-  },
-  {
-    key = "]",
-    mods = "CMD",
-    action = {
-      Multiple = {
-        { SendKey = { key = "b", mods = "CTRL" } },
-        { SendKey = { key = "l", mods = "CTRL" } },
-      },
-    },
-  },
+	{
+		key = "[",
+		mods = "CMD",
+		action = {
+			Multiple = { { SendKey = { key = "b", mods = "CTRL" } }, { SendKey = { key = "h", mods = "CTRL" } } },
+		},
+	},
+	{
+		key = "]",
+		mods = "CMD",
+		action = {
+			Multiple = { { SendKey = { key = "b", mods = "CTRL" } }, { SendKey = { key = "l", mods = "CTRL" } } },
+		},
+	},
+	{ key = "t", mods = "CMD", action = "DisableDefaultAssignment" },
+	{ key = "w", mods = "CMD", action = "DisableDefaultAssignment" },
 }
