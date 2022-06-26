e4989fc5dcd95861a5b548475678ccdccd73fb6a
Niels Mentink <niels@ment.ink>
2022-06-27 01:53:38 +0200

    Update dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -43,8 +43,8 @@ return {
 				Multiple = { { SendKey = { key = "b", mods = "CTRL" } }, { SendKey = { key = "l", mods = "CTRL" } } },
 			},
 		},
-		{ key = "t", mods = "CMD", action = "DisableDefaultAssignment" },
-		{ key = "w", mods = "CMD", action = "DisableDefaultAssignment" },
+		-- { key = "t", mods = "CMD", action = "DisableDefaultAssignment" },
+		-- { key = "w", mods = "CMD", action = "DisableDefaultAssignment" },
 		{
 			key = "Space",
 			mods = "CTRL",
