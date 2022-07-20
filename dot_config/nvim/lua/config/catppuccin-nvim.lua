1736959e84d2e392de0e83c0d0f331481be1b586
Niels Mentink <niels@ment.ink>
2022-07-20 18:29:33 +0200

    Update dot_config/nvim/lua/config/catppuccin-nvim.lua

--- dot_config/nvim/lua/config/catppuccin-nvim.lua
+++ dot_config/nvim/lua/config/catppuccin-nvim.lua
@@ -7,11 +7,11 @@ catppuccin.setup({
 	transparent_background = false,
 	term_colors = true,
 	styles = {
-		comments = "italic",
-		functions = "italic",
-		keywords = "italic",
-		strings = "NONE",
-		variables = "NONE",
+		comments = {"italic"},
+		functions = {"italic"},
+		keywords = {"italic"},
+		strings = {"NONE"},
+		variables = {"NONE"},
 	},
 })
 
