f5f929c9b22e73eb7956875e890bab169984a7b0
Niels Mentink <niels@ment.ink>
2022-08-01 02:36:10 +0200

    Add .luacheckrc Add .stylua.toml Update dot_config/nvim/lua/config/lsp/null-ls.lua

--- dot_config/nvim/lua/config/lsp/null-ls.lua
+++ dot_config/nvim/lua/config/lsp/null-ls.lua
@@ -1,12 +1,12 @@
 local null_ls = require("null-ls")
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name ~= "tsserver"
-    end,
-    bufnr = bufnr,
-  })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name ~= "tsserver"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -21,40 +21,39 @@ local code_actions = null_ls.builtins.code_actions
 local hover = null_ls.builtins.hover
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    code_actions.eslint_d,
-    code_actions.shellcheck,
-    formatting.eslint_d,
-    formatting.google_java_format,
-    formatting.phpcsfixer,
-    formatting.prettierd,
-    formatting.rubocop,
-    formatting.shfmt,
-    formatting.stylelint,
-    formatting.stylua,
-    formatting.tidy,
-    hover.dictionary,
-    linters.eslint_d,
-    linters.luacheck,
-    linters.selene,
-    linters.rubocop,
-    linters.shellcheck,
-    linters.stylelint,
-    linters.tidy,
-    linters.write_good,
-    linters.actionlint,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          lsp_formatting(bufnr)
-        end,
-      })
-    end
-  end,
+	debug = false,
+	sources = {
+		code_actions.eslint_d,
+		code_actions.shellcheck,
+		formatting.eslint_d,
+		formatting.google_java_format,
+		formatting.phpcsfixer,
+		formatting.prettierd,
+		formatting.rubocop,
+		formatting.shfmt,
+		formatting.stylelint,
+		formatting.stylua,
+		formatting.tidy,
+		hover.dictionary,
+		linters.eslint_d,
+		linters.luacheck,
+		linters.rubocop,
+		linters.shellcheck,
+		linters.stylelint,
+		linters.tidy,
+		linters.write_good,
+		linters.actionlint,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
