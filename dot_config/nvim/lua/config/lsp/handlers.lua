8415e0bb8dfcd1c3fb8cca1f584da2af15316dbc
Niels Mentink <niels@ment.ink>
2022-08-01 02:55:55 +0200

    Update .stylua.toml Update dot_config/nvim/lua/config/lsp/handlers.lua Update dot_config/nvim/lua/config/lsp/init.lua Update dot_config/nvim/lua/config/lsp/mason.lua

--- dot_config/nvim/lua/config/lsp/handlers.lua
+++ dot_config/nvim/lua/config/lsp/handlers.lua
@@ -4,75 +4,75 @@ M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_cmp_ok then
-	return
+  return
 end
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
 M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 
 M.setup = function()
-	local icons = require("config.icons")
-	local signs = {
-		{ name = "DiagnosticSignError", text = icons.Error },
-		{ name = "DiagnosticSignWarn", text = icons.Warning },
-		{ name = "DiagnosticSignHint", text = icons.Hint },
-		{ name = "DiagnosticSignInfo", text = icons.Information },
-	}
+  local icons = require("config.icons")
+  local signs = {
+    { name = "DiagnosticSignError", text = icons.Error },
+    { name = "DiagnosticSignWarn", text = icons.Warning },
+    { name = "DiagnosticSignHint", text = icons.Hint },
+    { name = "DiagnosticSignInfo", text = icons.Information },
+  }
 
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
 
-	vim.diagnostic.config({
-		virtual_lines = true,
-		virtual_text = false,
+  vim.diagnostic.config({
+    virtual_lines = false,
+    virtual_text = false,
 
-		signs = {
-			active = signs,
-		},
-		update_in_insert = false,
-		underline = true,
-		severity_sort = true,
-		float = {
-			focusable = true,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	})
+    signs = {
+      active = signs,
+    },
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = true,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  })
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
-	})
+  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = "rounded",
+  })
 end
 
 local function lsp_highlight_document(client)
-	local status_ok, illuminate = pcall(require, "illuminate")
-	if not status_ok then
-		return
-	end
-	illuminate.on_attach(client)
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+  illuminate.on_attach(client)
 end
 
 local function attach_navic(client, bufnr)
-	vim.g.navic_silence = true
-	local status_ok = pcall(require, "nvim-navic")
-	if not status_ok then
-		return
-	end
-	require("nvim-navic").attach(client, bufnr)
+  vim.g.navic_silence = true
+  local status_ok = pcall(require, "nvim-navic")
+  if not status_ok then
+    return
+  end
+  require("nvim-navic").attach(client, bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-	lsp_highlight_document(client)
-	attach_navic(client, bufnr)
+  lsp_highlight_document(client)
+  attach_navic(client, bufnr)
 
-	if client.name == "tsserver" then
-		require("lsp-inlayhints").on_attach(bufnr, client)
-	end
+  if client.name == "tsserver" then
+    require("lsp-inlayhints").on_attach(bufnr, client)
+  end
 end
 
 return M
--- dot_config/nvim/lua/config/lsp/mason.lua
+++ dot_config/nvim/lua/config/lsp/mason.lua
@@ -3,7 +3,7 @@ local mason_lsp_ok = pcall(require, "mason-lspconfig")
 local lsp_ok = pcall(require, "lspconfig")
 
 if not mason_ok and mason_lsp_ok and lsp_ok then
-	return
+  return
 end
 
 local mason = require("mason")
@@ -11,56 +11,54 @@ local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
 
 local settings = {
-	ui = {
-		border = "rounded",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
+  ui = {
+    border = "rounded",
+    icons = {
+      package_installed = "◍",
+      package_pending = "◍",
+      package_uninstalled = "◍",
+    },
+  },
+  log_level = vim.log.levels.INFO,
+  max_concurrent_installers = 4,
 }
 
 mason.setup(settings)
 mason_lspconfig.setup()
 
-local opts = {}
-
 for _, server in pairs(mason_lspconfig.get_installed_servers()) do
-	opts = {
-		on_attach = require("config.lsp.handlers").on_attach,
-		capabilities = require("config.lsp.handlers").capabilities,
-	}
+  local opts = {
+    on_attach = require("config.lsp.handlers").on_attach,
+    capabilities = require("config.lsp.handlers").capabilities,
+  }
 
-	server = vim.split(server, "@")[1]
+  server = vim.split(server, "@")[1]
 
-	if server == "jsonls" then
-		local jsonls_opts = require("config.lsp.settings.jsonls")
-		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	end
+  if server == "jsonls" then
+    local jsonls_opts = require("config.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts) or {}
+  end
 
-	if server == "yamlls" then
-		local yamlls_opts = require("config.lsp.settings.yamlls")
-		opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
-	end
+  if server == "yamlls" then
+    local yamlls_opts = require("config.lsp.settings.yamlls")
+    opts = vim.tbl_deep_extend("force", yamlls_opts, opts) or {}
+  end
 
-	if server == "sumneko_lua" then
-		local sumneko_lua_opts = require("config.lsp.settings.sumneko_lua")
-		opts = vim.tbl_deep_extend("force", sumneko_lua_opts, opts)
-		local l_status_ok = pcall(require, "lua-dev")
-		if not l_status_ok then
-			return
-		end
+  if server == "sumneko_lua" then
+    local sumneko_lua_opts = require("config.lsp.settings.sumneko_lua")
+    opts = vim.tbl_deep_extend("force", sumneko_lua_opts, opts) or {}
+    local l_status_ok = pcall(require, "lua-dev")
+    if not l_status_ok then
+      return
+    end
 
-		opts = require("lua-dev").setup({ lspconfig = opts })
-	end
+    opts = require("lua-dev").setup({ lspconfig = opts }) or {}
+  end
 
-	if server == "tsserver" then
-		local tsserver_opts = require("config.lsp.settings.tsserver")
-		opts = vim.tbl_deep_extend("force", tsserver_opts, opts)
-	end
+  if server == "tsserver" then
+    local tsserver_opts = require("config.lsp.settings.tsserver")
+    opts = vim.tbl_deep_extend("force", tsserver_opts, opts) or {}
+  end
 
-	lspconfig[server].setup(opts)
+  lspconfig[server].setup(opts)
 end
