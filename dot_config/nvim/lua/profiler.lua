95959afa728b0ecbb8640218aa8ec110b343a102
Niels Mentink <niels@ment.ink>
2022-08-17 22:04:44 +0200

    Update dot_config/nvim/lua/profiler.lua

--- dot_config/nvim/lua/profiler.lua
+++ dot_config/nvim/lua/profiler.lua
@@ -1,10 +1,5 @@
-local impatient_ok = pcall(require, "impatient")
+local impatient_ok, impatient = pcall(require, "impatient")
 if not impatient_ok then
-  return
-end
-
-local impatient = require("impatient")
-if not impatient then
   return
 end
 
