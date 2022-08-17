2c291d79b615590d940bde2c1c64ae64bfad8057
Niels Mentink <niels@ment.ink>
2022-08-17 10:26:17 +0200

    Update dot_config/nvim/lua/config/gitsigns.lua

--- dot_config/nvim/lua/config/gitsigns.lua
+++ dot_config/nvim/lua/config/gitsigns.lua
@@ -1,14 +1,17 @@
-local status_ok, gitsigns = pcall(require, "gitsigns")
+local status_ok = pcall(require, "gitsigns")
 if not status_ok then
   return
 end
+
+local gitsigns = require("gitsigns")
+local i = require("icons").unspaced
 
 gitsigns.setup({
   signs = {
     add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
     change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    delete = { hl = "GitSignsDelete", text = "契 ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契 ", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     changedelete = {
       hl = "GitSignsChange",
       text = "▎",
