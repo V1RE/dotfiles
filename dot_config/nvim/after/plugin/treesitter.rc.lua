5d65fe2f85e2fb61e8793f22df0d6c462df95519
Niels Mentink <niels@ment.ink>
2022-08-23 11:53:29 +0200

    Update dot_config/nvim/after/plugin/treesitter.rc.lua

--- dot_config/nvim/after/plugin/treesitter.rc.lua
+++ dot_config/nvim/after/plugin/treesitter.rc.lua
@@ -21,4 +21,13 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     enable_autocmd = false,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
 })
