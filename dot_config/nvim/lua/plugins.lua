e03d62f43707a34004f24b79d234cd4209b896a8
Niels Mentink <niels@ment.ink>
2022-08-25 01:23:11 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -25,9 +25,9 @@ local packer = require("packer")
 -- Have packer use a popup window
 packer.init({
   display = {
-    open_fn = function()
+    --[[ open_fn = function()
       return require("packer.util").float({ border = "rounded" })
-    end,
+    end, ]]
     keybindings = {
       quit = "<esc>",
     },
