8bf181afe848ac140fd9ebd284d5fe61e94b94df
Niels Mentink <niels@ment.ink>
2022-08-27 16:06:43 +0200

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -111,6 +111,7 @@ return packer.startup(function(use)
   use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
   use("nvim-telescope/telescope-ui-select.nvim")
   use("ThePrimeagen/refactoring.nvim")
+  use("nvim-telescope/telescope-file-browser.nvim")
 
   -- Treesitter
   use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
