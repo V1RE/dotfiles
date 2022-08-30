117efe42507341c8364f6ac7e62665843ead42b3
Niels Mentink <niels@ment.ink>
2022-08-30 13:43:00 +0200

    Update dot_config/nvim/after/plugin/cmp.rc.lua

--- dot_config/nvim/after/plugin/cmp.rc.lua
+++ dot_config/nvim/after/plugin/cmp.rc.lua
@@ -40,6 +40,7 @@ cmp.setup({
     end,
   },
   sources = {
+    { name = "nvim_lsp_signature_help" },
     { name = "copilot" },
     { name = "luasnip", max_item_count = 3 },
     { name = "nvim_lsp" },
