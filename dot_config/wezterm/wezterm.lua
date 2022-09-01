0a5f447d53c911ebe3636bf31b2f7111a42506f8
Niels Mentink <niels@ment.ink>
2022-09-01 23:08:45 +0200

    Update dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -19,17 +19,18 @@ return {
   font_dirs = { "fonts" },
   font = font_with_fallback("Cascadia Code"),
   font_size = 18,
-  color_scheme = "catppuccin",
-  colors = require("colors.catppuccin"),
+  line_height = 1.4,
+  color_scheme = "Poimandres",
+  --[[ colors = require("colors.catppuccin"), ]]
   hide_tab_bar_if_only_one_tab = true,
   term = "wezterm",
   window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    left = 30,
+    right = 30,
+    top = 30,
+    bottom = 30,
   },
-  use_fancy_tab_bar = false,
+  use_fancy_tab_bar = true,
   tab_bar_at_bottom = true,
   keys = {
     {
