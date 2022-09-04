29ab7b1e704672c474ac9b6030451477d972be3b
Niels Mentink <niels@ment.ink>
2022-09-04 20:06:50 +0200

    Update dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -49,7 +49,7 @@ return {
       key = "Enter",
       mods = "CTRL",
       action = act.ShowLauncherArgs({
-        flags = "FUZZY|COMMANDS",
+        flags = "FUZZY|WORKSPACES",
       }),
     },
     {
