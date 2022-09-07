ab9f74becede12b65ee4d07fd3c0cbcfbde104b5
Niels Mentink <niels@ment.ink>
2022-09-07 09:46:24 +0200

    Update dot_config/nvim/after/plugin/indentline.rc.lua

--- dot_config/nvim/after/plugin/indentline.rc.lua
+++ dot_config/nvim/after/plugin/indentline.rc.lua
@@ -58,7 +58,7 @@ require("indent_blankline").setup({
   -- space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
-  char_highlight_list = {
+  context_highlight_list = {
     "IndentBlanklineIndent6",
     "IndentBlanklineIndent5",
     "IndentBlanklineIndent4",
