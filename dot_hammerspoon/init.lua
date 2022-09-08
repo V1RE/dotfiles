0c2fd1780498fb04356c8897627c80be2f044ef0
Niels Mentink <niels@ment.ink>
2022-09-08 10:33:55 +0200

    Update dot_hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,11 +1,11 @@
 hs.loadSpoon("ControlEscape"):start()
 
---[[ local switcher = hs.window.switcher.new({ "Arc", "WezTerm" })
+--[[ local switcher = hs.window.switcher.new({ "Arc", "WezTerm" }) ]]
 
 hs.hotkey.bind({ "cmd", "ctrl" }, "l", function()
-  switcher:next()
+  hs.application.open("Arc")
 end)
 
 hs.hotkey.bind({ "cmd", "ctrl" }, "h", function()
-  switcher:previous()
-end) ]]
+  hs.application.open("WezTerm")
+end)
