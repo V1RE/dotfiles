d925fc7c727bd51f7c8ba14a0e3886bb6ec9679d
Niels Mentink <niels@ment.ink>
2022-09-08 11:30:48 +0200

    Update dot_hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -2,10 +2,18 @@ hs.loadSpoon("ControlEscape"):start()
 
 --[[ local switcher = hs.window.switcher.new({ "Arc", "WezTerm" }) ]]
 
+local function centerMouse()
+  local screen = hs.screen.primaryScreen()
+  local center = screen:fullFrame().center
+  hs.mouse.setAbsolutePosition(center)
+end
+
 hs.hotkey.bind({ "cmd", "ctrl" }, "l", function()
+  centerMouse()
   hs.application.get("Arc"):activate(true)
 end)
 
 hs.hotkey.bind({ "cmd", "ctrl" }, "h", function()
+  centerMouse()
   hs.application.open("WezTerm")
 end)
