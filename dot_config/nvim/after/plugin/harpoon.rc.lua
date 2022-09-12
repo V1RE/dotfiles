6cbdaeae64e6dc3b8651365632fcb9c2713b6618
Niels Mentink <niels@ment.ink>
2022-09-12 23:08:37 +0200

    Update dot_config/nvim/after/plugin/harpoon.rc.lua

--- dot_config/nvim/after/plugin/harpoon.rc.lua
+++ dot_config/nvim/after/plugin/harpoon.rc.lua
@@ -1,17 +1,27 @@
 local wk = require("which-key")
 local ui = require("harpoon.ui")
+local mark = require("harpoon.mark")
 local icons = require("config.icons")
 
 require("harpoon").setup()
 
--- vim.pretty_print(vim.fn.fnamemodify(require("harpoon.mark").get_marked_file_name(1), ":t"))
+--- @param index number
+local function getMarkName(index)
+  local name = mark.get_marked_file_name(index)
+
+  if name then
+    return vim.fn.fnamemodify(name, ":t")
+  end
+  return "Mark " .. index
+end
+
 --- @param index number
 local function nav_mark(index)
   return {
     function()
       ui.nav_file(index)
     end,
-    "Mark " .. index,
+    getMarkName(index),
   }
 end
 
@@ -19,10 +29,21 @@ wk.register({
   [";"] = {
     name = icons.BookMark .. "Harpoon",
     j = { ui.toggle_quick_menu, "Quick menu" },
-    c = { require("harpoon.mark").add_file, "Add mark" },
+    c = { mark.add_file, "Add mark" },
     a = nav_mark(1),
     s = nav_mark(2),
     d = nav_mark(3),
     f = nav_mark(4),
   },
 })
+
+mark.on("changed", function()
+  wk.register({
+    [";"] = {
+      a = getMarkName(1),
+      s = getMarkName(2),
+      d = getMarkName(3),
+      f = getMarkName(4),
+    },
+  })
+end)
