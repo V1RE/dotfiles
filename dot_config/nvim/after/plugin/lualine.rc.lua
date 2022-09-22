25d74abf4ce4f8a22d60a1130cad310cef7788d4
Niels Mentink <niels@ment.ink>
2022-09-22 19:17:52 +0200

    Update dot_config/nvim/after/plugin/lualine.rc.lua

--- dot_config/nvim/after/plugin/lualine.rc.lua
+++ dot_config/nvim/after/plugin/lualine.rc.lua
@@ -88,7 +88,7 @@ end
 require("lualine").setup({
   options = {
     icons_enabled = true,
-    theme = "poimandres",
+    theme = "catppuccin",
     component_separators = { left = "", right = "" },
     section_separators = { left = "", right = "" },
     disabled_filetypes = {},
