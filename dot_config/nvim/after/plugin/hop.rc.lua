aba6e5ab499cb6955843ef7b733f0f94c5248052
Niels Mentink <niels@ment.ink>
2022-09-25 18:24:06 +0200

    Update dot_config/nvim/after/plugin/hop.rc.lua

--- dot_config/nvim/after/plugin/hop.rc.lua
+++ dot_config/nvim/after/plugin/hop.rc.lua
@@ -1,7 +1,18 @@
 local hop = require("hop")
 local icons = require("config.icons")
 local utils = require("utils")
+local hd = require("hop.hint").HintDirection
 
 hop.setup()
 
-utils.map({ ["<TAB>"] = { hop.hint_words, icons.Rocket .. "Hop" } })
+local function f()
+  hop.hint_char1({
+    direction = hd.AFTER_CURSOR,
+    current_line_only = true,
+  })
+end
+
+utils.map({
+  ["<TAB>"] = { hop.hint_words, icons.Rocket .. "Hop" },
+  f = { f, "lol" },
+})
