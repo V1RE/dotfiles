d6f74b08231bf1c0f68e3c2c9e27f2ccbb96ef77
Niels Mentink <niels@ment.ink>
2022-09-30 01:34:55 +0200

    Update dot_config/nvim/after/plugin/feline.rc.lua

--- dot_config/nvim/after/plugin/feline.rc.lua
+++ dot_config/nvim/after/plugin/feline.rc.lua
@@ -1,16 +1,18 @@
 local navic = require("nvim-navic")
 
 local components = {
-  active = {
-    {
-      provider = function()
-        return navic.get_location()
-      end,
-      enabled = function()
-        return navic.is_available()
-      end,
-    },
-  },
+  active = { {} },
+  inactive = {},
 }
 
-require("feline").setup({ components = components })
+table.insert(components.active[1], {
+  provider = function()
+    return "asdf"
+    --[[ return navic.get_location() ]]
+  end,
+  --[[ enabled = function() ]]
+  --[[   return navic.is_available() ]]
+  --[[ end, ]]
+})
+
+require("feline").winbar.setup({ components = components })
