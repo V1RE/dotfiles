1b4420448667348a8e971969cc25933293b64df4
Niels Mentink <niels@ment.ink>
2022-10-02 15:53:28 +0200

    Update dot_config/nvim/after/plugin/aerial.rc.lua

--- dot_config/nvim/after/plugin/aerial.rc.lua
+++ dot_config/nvim/after/plugin/aerial.rc.lua
@@ -1,7 +1,7 @@
 local aerial = require("aerial")
 -- Call the setup function to change the default behavior
 aerial.setup({
-  open_automatic = function()
-    return not aerial.was_closed()
+  open_automatic = function(bufnr)
+    return not aerial.was_closed() and aerial.num_symbols(bufnr) > 1
   end,
 })
