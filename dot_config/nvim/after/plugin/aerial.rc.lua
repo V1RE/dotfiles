f7e44f97dc02547574a9b8f68da8e8b37ab69b7e
Niels Mentink <niels@ment.ink>
2022-10-02 16:07:19 +0200

    Update dot_config/nvim/after/plugin/aerial.rc.lua

--- dot_config/nvim/after/plugin/aerial.rc.lua
+++ dot_config/nvim/after/plugin/aerial.rc.lua
@@ -1,7 +1,30 @@
 local aerial = require("aerial")
 -- Call the setup function to change the default behavior
 aerial.setup({
-  open_automatic = function(bufnr)
-    return not aerial.was_closed() and aerial.num_symbols(bufnr) > 0
-  end,
+  attach_mode = "global",
+
+  layout = {
+    -- These control the width of the aerial window.
+    -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+    -- min_width and max_width can be a list of mixed types.
+    -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+    max_width = { 40, 0.2 },
+    width = nil,
+    min_width = 10,
+
+    -- Determines the default direction to open the aerial window. The 'prefer'
+    -- options will open the window in the other direction *if* there is a
+    -- different buffer in the way of the preferred direction
+    -- Enum: prefer_right, prefer_left, right, left, float
+    default_direction = "right",
+
+    -- Determines where the aerial window will be opened
+    --   edge   - open aerial at the far right/left of the editor
+    --   window - open aerial to the right/left of the current window
+    placement = "edge",
+  },
+
+  open_automatic = true,
+
+  close_automatic_events = { "unsupported" },
 })
