278206cf448f8d8d8927b09943aac5e115dd216b
Niels Mentink <niels@ment.ink>
2022-10-04 09:47:29 +0200

    Update dot_config/nvim/after/plugin/aerial.rc.lua

--- dot_config/nvim/after/plugin/aerial.rc.lua
+++ dot_config/nvim/after/plugin/aerial.rc.lua
@@ -9,14 +9,14 @@ aerial.setup({
     -- min_width and max_width can be a list of mixed types.
     -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
     max_width = { 40, 0.2 },
-    width = nil,
+    width = 40,
     min_width = 10,
 
     -- Determines the default direction to open the aerial window. The 'prefer'
     -- options will open the window in the other direction *if* there is a
     -- different buffer in the way of the preferred direction
     -- Enum: prefer_right, prefer_left, right, left, float
-    default_direction = "right",
+    default_direction = "left",
     placement = "edge",
   },
 
