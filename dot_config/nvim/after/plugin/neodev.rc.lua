927acfa0ea0089e1abbc6dbd26db5d0b6e4edf00
Niels Mentink <niels@ment.ink>
2022-10-17 10:50:14 +0200

    Update .config/nvim/after/plugin/neodev.rc.lua

--- dot_config/nvim/after/plugin/neodev.rc.lua
+++ dot_config/nvim/after/plugin/neodev.rc.lua
@@ -1,1 +1,8 @@
-require("neodev").setup({})
+require("neodev").setup({
+  override = function(root_dir, library)
+    if require("neodev.util").has_file(root_dir, "/Users/niels/.local/share/chezmoi") then
+      library.enabled = true
+      library.plugins = true
+    end
+  end,
+})
