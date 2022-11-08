9be52de7d0d20f2b656403a2e54b624d3f623ae9
Niels Mentink <niels@ment.ink>
2022-11-08 14:46:58 +0100

    Update .config/nvim/after/plugin/nvim-tree.rc.lua

--- dot_config/nvim/after/plugin/nvim-tree.rc.lua
+++ dot_config/nvim/after/plugin/nvim-tree.rc.lua
@@ -6,11 +6,9 @@ require("nvim-tree").setup({
   open_on_setup = true,
   open_on_setup_file = true,
   hijack_cursor = true,
-  disable_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
+  hijack_unnamed_buffer_when_opening = true,
   diagnostics = {
     enable = true,
-    show_on_dirs = true,
     icons = {
       error = i.Error,
       hint = i.Hint,
