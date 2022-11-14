55f795b4d5ddd3dd79b7fe4780ee5058a2e2899a
Niels Mentink <niels@ment.ink>
2022-11-14 08:25:49 +0100

    Remove .config/nvim/after/plugin/refactoring.rc.lua Remove .config/nvim/after/plugin/window-picker.rc.lua Remove .config/nvim/lua/colorscheme.lua Remove .config/nvim/lua/config/copilot.rc.lua Remove .config/nvim/lua/config/winbar.lua

--- dot_config/nvim/lua/config/copilot.rc.lua
+++
@@ -1,7 +0,0 @@
-local copilot = require("copilot")
-
-vim.defer_fn(function()
-  copilot.setup({
-    copilot_node_command = "/Users/niels/.volta/tools/image/node/16.17.0/bin/node",
-  })
-end, 100)
