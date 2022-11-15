3e7f0d47d2e626b80d6091c92a9b7397565cddb1
Niels Mentink <niels@ment.ink>
2022-11-15 14:40:39 +0100

    Update .config/nvim/after/plugin/ssr.rc.lua

--- dot_config/nvim/after/plugin/ssr.rc.lua
+++ dot_config/nvim/after/plugin/ssr.rc.lua
@@ -1,1 +1,12 @@
-require("ssr").setup({})
+local ssr = require("ssr")
+local util = require("utils")
+
+ssr.setup()
+
+util.map({
+  ["<leader>"] = {
+    l = {
+      R = { ssr.open, "SSR" },
+    },
+  },
+})
