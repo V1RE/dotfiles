c6f6c7e6e623b7f62c90219389b132700fc10923
Niels Mentink <niels@ment.ink>
2022-11-22 14:05:12 +0100

    Update .config/nvim/after/plugin/colorizer.rc.lua

--- dot_config/nvim/after/plugin/colorizer.rc.lua
+++ dot_config/nvim/after/plugin/colorizer.rc.lua
@@ -1,13 +1,13 @@
 require("colorizer").setup({
   filetypes = { "*", "!cmp_menu" },
   user_default_options = {
-    RRGGBBAA = true, -- #RRGGBBAA hex codes
-    AARRGGBB = true, -- #0xAARRGGBA hex codes
+    RRGGBBAA = false, -- #RRGGBBAA hex codes
+    AARRGGBB = false, -- #0xAARRGGBA hex codes
     css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
     -- Available modes: foreground, background
-    mode = "virtualtext", -- Set the display mode.
+    mode = "background", -- Set the display mode.
     tailwind = false,
     sass = { enable = true },
   },
-  buftypes = { "*", "!terminal", "!prompt", "!popup" },
+  buftypes = { "*", "!terminal", "!prompt", "!popup", "!nofile" },
 })
