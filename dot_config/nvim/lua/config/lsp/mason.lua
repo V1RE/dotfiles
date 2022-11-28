adb1e05d67c807733cb46620368c9b2ee042150a
Niels Mentink <niels@ment.ink>
2022-11-28 16:34:31 +0100

    Update .config/nvim/lua/config/lsp/mason.lua Add .config/nvim/lua/config/lsp/null-ls.lua Update .config/nvim/lua/config/lsp/settings/sumneko_lua.lua

--- dot_config/nvim/lua/config/lsp/mason.lua
+++ dot_config/nvim/lua/config/lsp/mason.lua
@@ -3,64 +3,64 @@ local mason_lspconfig = require("mason-lspconfig")
 local lspconfig = require("lspconfig")
 
 local settings = {
-  ui = {
-    border = "rounded",
-    icons = {
-      package_installed = "◍",
-      package_pending = "◍",
-      package_uninstalled = "◍",
-    },
-  },
-  log_level = vim.log.levels.INFO,
-  max_concurrent_installers = 4,
+	ui = {
+		border = "rounded",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
 mason.setup(settings)
 mason_lspconfig.setup()
 
 local opts = {
-  on_attach = require("config.lsp.handlers").on_attach,
-  capabilities = require("config.lsp.handlers").capabilities,
+	on_attach = require("config.lsp.handlers").on_attach,
+	capabilities = require("config.lsp.handlers").capabilities,
 }
 
 --- @param server string
 local function merge_options(server)
-  local server_options = require("config.lsp.settings." .. server) or {}
+	local server_options = require("config.lsp.settings." .. server) or {}
 
-  return vim.tbl_deep_extend("keep", server_options, opts)
+	return vim.tbl_deep_extend("keep", server_options, opts)
 end
 
 mason_lspconfig.setup_handlers({
-  function(server)
-    lspconfig[server].setup(opts)
-  end,
+	function(server)
+		lspconfig[server].setup(opts)
+	end,
 
-  ["sumneko_lua"] = function(server)
-    require("neodev").setup({
-      override = function(root_dir, library)
-        if require("neodev.util").has_file(root_dir, "~/.local/share/chezmoi") then
-          library.enabled = true
-          library.plugins = true
-        end
-      end,
-    })
+	["sumneko_lua"] = function(server)
+		require("neodev").setup({
+			override = function(root_dir, library)
+				if require("neodev.util").has_file(root_dir, "~/.local/share/chezmoi") then
+					library.enabled = true
+					library.plugins = true
+				end
+			end,
+		})
 
-    lspconfig.sumneko_lua.setup(merge_options(server))
-  end,
+		lspconfig.sumneko_lua.setup(merge_options(server))
+	end,
 
-  ["jsonls"] = function(server)
-    lspconfig.jsonls.setup(merge_options(server))
-  end,
+	["jsonls"] = function(server)
+		lspconfig.jsonls.setup(merge_options(server))
+	end,
 
-  ["yamlls"] = function(server)
-    lspconfig.yamlls.setup(merge_options(server))
-  end,
+	["yamlls"] = function(server)
+		lspconfig.yamlls.setup(merge_options(server))
+	end,
 
-  ["tsserver"] = function(server)
-    require("typescript").setup({ server = merge_options(server) })
-  end,
+	["tsserver"] = function(server)
+		require("typescript").setup({ server = merge_options(server) })
+	end,
 
-  --[[ ["jdtls"] = function(_)
+	--[[ ["jdtls"] = function(_)
     require("jdtls").start_or_attach(vim.tbl_deep_extend("force", opts, require("config.lsp.settings.jdtls")) or {})
   end, ]]
 })
--- dot_config/nvim/lua/config/lsp/settings/sumneko_lua.lua
+++ dot_config/nvim/lua/config/lsp/settings/sumneko_lua.lua
@@ -1,21 +1,18 @@
 return {
-  settings = {
-    Lua = {
-      hint = {
-        enable = true,
-      },
-      format = {
-        enable = false,
-      },
-      diagnostics = {
-        globals = { "vim", "hs" },
-      },
-      telemetry = {
-        enable = false,
-      },
-      --[[ workspace = {
-        library = { "~/.hammerspoon/Spoons/EmmyLua.spoon/annotations" },
-      }, ]]
-    },
-  },
+	settings = {
+		Lua = {
+			hint = {
+				enable = true,
+			},
+			format = {
+				enable = false,
+			},
+			diagnostics = {
+				globals = { "vim", "hs" },
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 }
