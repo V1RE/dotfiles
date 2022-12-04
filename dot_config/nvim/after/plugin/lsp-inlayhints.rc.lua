9c8def23033c81381e6b3d529c3d6ad219c58861
Niels Mentink <niels@ment.ink>
2022-12-04 18:22:52 +0100

    Update .config/nvim/after/plugin/lsp-inlayhints.rc.lua

--- dot_config/nvim/after/plugin/lsp-inlayhints.rc.lua
+++ dot_config/nvim/after/plugin/lsp-inlayhints.rc.lua
@@ -1,1 +1,20 @@
-local hints = require("lsp-inlayhints")
+local status, hints = pcall(require, "lsp-inlayhints")
+if not status then
+  return
+end
+
+hints.setup({})
+
+vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = "LspAttach_inlayhints",
+  callback = function(args)
+    if not (args.data and args.data.client_id) then
+      return
+    end
+
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    hints.on_attach(client, bufnr, false)
+  end,
+})
