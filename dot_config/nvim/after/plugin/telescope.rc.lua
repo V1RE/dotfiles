701d44ff76d7b65f68e968ec7eba26784c1899bc
Niels Mentink <niels@ment.ink>
2022-12-05 09:36:59 +0100

    Update .config/nvim/after/plugin/telescope.rc.lua

--- dot_config/nvim/after/plugin/telescope.rc.lua
+++ dot_config/nvim/after/plugin/telescope.rc.lua
@@ -48,12 +48,6 @@ telescope.setup({
   },
 
   extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case",
-    },
     file_browser = {
       theme = "dropdown",
       hijack_netrw = false,
@@ -79,7 +73,6 @@ telescope.setup({
   },
 })
 
---[[ telescope.load_extension("fzf") ]]
 telescope.load_extension("ui-select")
 telescope.load_extension("projects")
 telescope.load_extension("file_browser")
