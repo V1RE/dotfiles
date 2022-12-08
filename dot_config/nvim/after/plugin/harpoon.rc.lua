0a5b931493073da336f3842c632fdb7fbf10bd63
Niels Mentink <niels@ment.ink>
2022-12-08 10:21:56 +0100

    Update .config/nvim/after/plugin/comment.rc.lua Update .config/nvim/after/plugin/dial.rc.lua Update .config/nvim/after/plugin/gitsigns.rc.lua Update .config/nvim/after/plugin/harpoon.rc.lua Update .config/nvim/after/plugin/null-ls.rc.lua

--- dot_config/nvim/after/plugin/harpoon.rc.lua
+++ dot_config/nvim/after/plugin/harpoon.rc.lua
@@ -7,43 +7,43 @@ require("harpoon").setup()
 
 --- @param index number
 local function getMarkName(index)
-  local name = mark.get_marked_file_name(index)
+	local name = mark.get_marked_file_name(index)
 
-  if name then
-    return vim.fn.fnamemodify(name, ":t")
-  end
-  return "Mark " .. index
+	if name then
+		return vim.fn.fnamemodify(name, ":t")
+	end
+	return "Mark " .. index
 end
 
 --- @param index number
 local function nav_mark(index)
-  return {
-    function()
-      ui.nav_file(index)
-    end,
-    getMarkName(index),
-  }
+	return {
+		function()
+			ui.nav_file(index)
+		end,
+		getMarkName(index),
+	}
 end
 
 wk.register({
-  [";"] = {
-    name = icons.BookMark .. "Harpoon",
-    j = { ui.toggle_quick_menu, "Quick menu" },
-    c = { mark.add_file, "Add mark" },
-    a = nav_mark(1),
-    s = nav_mark(2),
-    d = nav_mark(3),
-    f = nav_mark(4),
-  },
+	[";"] = {
+		name = icons.BookMark .. "Harpoon",
+		j = { ui.toggle_quick_menu, "Quick menu" },
+		c = { mark.add_file, "Add mark" },
+		a = nav_mark(1),
+		s = nav_mark(2),
+		d = nav_mark(3),
+		f = nav_mark(4),
+	},
 })
 
 mark.on("changed", function()
-  wk.register({
-    [";"] = {
-      a = getMarkName(1),
-      s = getMarkName(2),
-      d = getMarkName(3),
-      f = getMarkName(4),
-    },
-  })
+	wk.register({
+		[";"] = {
+			a = getMarkName(1),
+			s = getMarkName(2),
+			d = getMarkName(3),
+			f = getMarkName(4),
+		},
+	})
 end)
