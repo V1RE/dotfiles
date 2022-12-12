53f0f1b79bfef9b3026140e5da2fd1343f737d4c
Niels Mentink <niels@ment.ink>
2022-12-12 09:38:22 +0100

    Update .config/nvim/lua/config/lsp/settings/tsserver.lua

--- dot_config/nvim/lua/config/lsp/settings/tsserver.lua
+++ dot_config/nvim/lua/config/lsp/settings/tsserver.lua
@@ -5,22 +5,7 @@ local tsserver = {
   root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
   settings = {
     typescript = {
-      inlayHints = {
-        functionLikeReturnTypes = { enabled = true },
-        enumMemberValues = { enabled = true },
-        parameterNames = { enabled = "all" },
-        parameterTypes = { enabled = true },
-        propertyDeclarationTypes = { enabled = true },
-        variableTypes = { enabled = true },
-      },
       format = { enable = false },
-      referencesCodeLens = { enabled = true },
-      updateImportsOnFileMove = {
-        enabled = "always",
-      },
-      implementationsCodeLens = {
-        enabled = true,
-      },
     },
   },
 }
