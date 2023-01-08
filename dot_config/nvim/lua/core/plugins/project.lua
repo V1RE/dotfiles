6bc4dffe62e3e49498495d88daf4e883e8998ee6
Niels Mentink <niels@ment.ink>
2023-01-09 00:55:21 +0100

    Update .config/nvim/lua/core/plugins/project.lua

--- dot_config/nvim/lua/core/plugins/project.lua
+++ dot_config/nvim/lua/core/plugins/project.lua
@@ -2,6 +2,10 @@
 local M = {
   {
     "ahmedkhalf/project.nvim",
+    ---@param opts ProjectOptions
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+    end,
     ---@type ProjectOptions
     opts = {
       silent_chdir = true,
