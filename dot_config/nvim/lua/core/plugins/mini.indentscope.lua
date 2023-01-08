7b53d1fde05c66f9d6ad73ed2e76be47e2de5c8b
Niels Mentink <niels@ment.ink>
2023-01-09 00:33:18 +0100

    Add .config/nvim/lua/core/plugins/mini.indentscope.lua

---
+++ dot_config/nvim/lua/core/plugins/mini.indentscope.lua
@@ -0,0 +1,23 @@
+---@type LazyPluginSpec[]
+local M = {
+  {
+    "echasnovski/mini.indentscope",
+    event = "BufReadPre",
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    config = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+      require("mini.indentscope").setup(opts)
+    end,
+  },
+}
+
+return M
