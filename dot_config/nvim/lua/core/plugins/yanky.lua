ade01deb59b7cd3b6a1541ce8f19f22541135e8d
Niels Mentink <niels@ment.ink>
2023-01-09 00:59:09 +0100

    Update .config/nvim/lua/core/plugins/yanky.lua

--- dot_config/nvim/lua/core/plugins/yanky.lua
+++ dot_config/nvim/lua/core/plugins/yanky.lua
@@ -1,16 +1,22 @@
-local yanky = require("yanky")
-
-yanky.setup({
-  highlight = {
-    on_put = true,
-    on_yank = true,
-    timer = 500,
-  },
-})
-
 vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
 vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
 vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
 vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
 vim.keymap.set("n", "<c-n>", "<Plug>(YankyCycleForward)")
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)")
+
+---@type LazyPluginSpec[]
+local M = {
+  {
+    "gbprod/yanky.nvim",
+    opts = {
+      highlight = {
+        on_put = true,
+        on_yank = true,
+        timer = 500,
+      },
+    },
+  },
+}
+
+return M
