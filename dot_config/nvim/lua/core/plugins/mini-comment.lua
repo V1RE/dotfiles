174ccf9a48fc93813233e6e84a6c49987d465be0
Niels Mentink <niels@ment.ink>
2023-01-10 01:31:33 +0100

    Update .config/nvim/.lazy-lock.json Add .config/nvim/lua/core/plugins/mini-comment.lua Update .config/nvim/lua/core/plugins/treesitter.lua

---
+++ dot_config/nvim/lua/core/plugins/mini-comment.lua
@@ -0,0 +1,20 @@
+---@type LazyPluginSpec[]
+local M = {
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
+  {
+    "echasnovski/mini.comment",
+    event = "VeryLazy",
+    opts = {
+      hooks = {
+        pre = function()
+          require("ts_context_commentstring.internal").update_commentstring({})
+        end,
+      },
+    },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
+}
+
+return M
