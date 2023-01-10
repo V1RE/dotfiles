3607c1a4f949573520a1f4b0e720e2dcdd543b89
Niels Mentink <niels@ment.ink>
2023-01-10 01:50:24 +0100

    Add .config/nvim/lua/core/plugins/mini-pairs.lua

---
+++ dot_config/nvim/lua/core/plugins/mini-pairs.lua
@@ -0,0 +1,12 @@
+---@type LazyPluginSpec[]
+local M = {
+  {
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
+}
+
+return M
