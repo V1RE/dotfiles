60cb1527154f6bd8eeb4dc5aaa16abb889850016
Niels Mentink <niels@ment.ink>
2023-01-10 14:17:36 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/noice.lua

--- dot_config/nvim/lua/core/plugins/noice.lua
+++ dot_config/nvim/lua/core/plugins/noice.lua
@@ -16,6 +16,7 @@ local M = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
+        lsp_doc_border = true,
       },
     },
     -- stylua: ignore
