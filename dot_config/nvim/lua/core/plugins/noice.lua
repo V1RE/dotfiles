98288dc0dbc2b9738da9a2792205d159265f34fb
Niels Mentink <niels@ment.ink>
2023-01-10 14:23:10 +0100

    Update .config/nvim/lua/core/plugins/noice.lua Update .config/nvim/lua/core/plugins/nvim-notify.lua

--- dot_config/nvim/lua/core/plugins/noice.lua
+++ dot_config/nvim/lua/core/plugins/noice.lua
@@ -18,7 +18,6 @@ local M = {
         command_palette = true,
         long_message_to_split = true,
         lsp_doc_border = true,
-        inc_rename = true,
       },
     },
     -- stylua: ignore
--- dot_config/nvim/lua/core/plugins/nvim-notify.lua
+++ dot_config/nvim/lua/core/plugins/nvim-notify.lua
@@ -8,7 +8,6 @@ local M = {
     },
     ---@type notify.Config
     opts = {
-      level = 3,
       timeout = 3000,
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
