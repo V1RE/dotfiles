55082954168f6d96970307545b30d0cd32f64ece
Niels Mentink <niels@ment.ink>
2023-01-10 02:01:32 +0100

    Update .config/nvim/lua/core/plugins/mini-surround.lua

--- dot_config/nvim/lua/core/plugins/mini-surround.lua
+++ dot_config/nvim/lua/core/plugins/mini-surround.lua
@@ -2,8 +2,20 @@
 local M = {
   {
     "echasnovski/mini.surround",
-    keys = { "s" },
+    keys = { "gz" },
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
     config = function(_, opts)
+      -- use gz mappings instead of s to prevent conflict with leap
       require("mini.surround").setup(opts)
     end,
   },
