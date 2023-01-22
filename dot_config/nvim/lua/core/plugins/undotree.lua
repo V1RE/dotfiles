40a95e48ec45cf26ec00849b2ed2ae38d460321d
Niels Mentink <niels@ment.ink>
2023-01-22 22:35:12 +0100

    Update .config/nvim/lua/core/plugins/undotree.lua

--- dot_config/nvim/lua/core/plugins/undotree.lua
+++ dot_config/nvim/lua/core/plugins/undotree.lua
@@ -3,7 +3,7 @@ local i = require("config.icons")
 ---@type LazyPluginSpec
 local M = {
   "mbbill/undotree",
-  keys = { { "n", "<leader>u", "<cmd>UndotreeToggle<cr>", desc = i.History .. "Undotree" } },
+  keys = { { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = i.History .. "Undotree" } },
 }
 
 return M
