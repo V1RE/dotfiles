517bc60e10d162785b5f313d4deec53504f86a99
Niels Mentink <niels@ment.ink>
2023-01-24 19:42:14 +0100

    Update .config/nvim/lua/core/plugins/chezmoi.lua

--- dot_config/nvim/lua/core/plugins/chezmoi.lua
+++ dot_config/nvim/lua/core/plugins/chezmoi.lua
@@ -3,7 +3,7 @@ local M = {
   {
     "Lilja/vim-chezmoi",
     cond = function()
-      return nil ~= require("core.util").get_root():find(".local/chezmoi")
+      return nil ~= require("core.util").get_root():find(".local/share/chezmoi")
     end,
     init = function()
       vim.g.chezmoi = "enabled"
@@ -12,7 +12,7 @@ local M = {
   {
     "alker0/chezmoi.vim",
     cond = function()
-      return nil ~= require("core.util").get_root():find(".local/chezmoi")
+      return nil ~= require("core.util").get_root():find(".local/share/chezmoi")
     end,
   },
 }
