d5508fe0d2751b6df14aaf58e92e299652f30c81
Niels Mentink <niels@ment.ink>
2023-01-24 21:18:24 +0100

    Update .config/nvim/lua/core/plugins/lsp/init.lua

--- dot_config/nvim/lua/core/plugins/lsp/init.lua
+++ dot_config/nvim/lua/core/plugins/lsp/init.lua
@@ -2,7 +2,13 @@
 local M = {
   {
     "marilari88/twoslash-queries.nvim",
-    config = true,
+    config = function()
+      local tsq = require("twoslash-queries")
+
+      tsq.setup()
+
+      require("core.util").on_attach(tsq.attach)
+    end,
     ft = { "typescript", "typescriptreact" },
   },
 }
