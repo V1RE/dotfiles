6502586c6f273376b98ffe472be337dff07bf55c
Niels Mentink <niels@ment.ink>
2023-01-24 21:14:01 +0100

    Add .config/nvim/lua/core/plugins/lsp/init.lua Remove .config/nvim/lua/core/plugins/lsp/typescript/twoslash-queries.lua

---
+++ dot_config/nvim/lua/core/plugins/lsp/init.lua
@@ -0,0 +1,10 @@
+---@type LazyPluginSpec[]
+local M = {
+  {
+    "marilari88/twoslash-queries.nvim",
+    config = true,
+    ft = { "typescript", "typescriptreact" },
+  },
+}
+
+return M
--- dot_config/nvim/lua/core/plugins/lsp/typescript/twoslash-queries.lua
+++
@@ -1,8 +0,0 @@
----@type LazyPluginSpec
-local M = {
-  "marilari88/twoslash-queries.nvim",
-  config = true,
-  ft = { "typescript", "typescriptreact" },
-}
-
-return M
