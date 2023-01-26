34f7a0dc0ca522d72023e94b9a84272e4aea37c5
Niels Mentink <niels@ment.ink>
2023-01-26 10:33:59 +0100

    Update .config/nvim/after/plugin/null-ls.rc.lua

--- dot_config/nvim/after/plugin/null-ls.rc.lua
+++ dot_config/nvim/after/plugin/null-ls.rc.lua
@@ -1,5 +1,18 @@
 local null_ls = require("null-ls")
 
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name ~= "tsserver"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local linters = null_ls.builtins.diagnostics
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#code-actions
@@ -13,13 +26,13 @@ null_ls.setup({
     code_actions.eslint_d,
     code_actions.shellcheck,
     require("typescript.extensions.null-ls.code-actions"),
-    null_ls.builtins.formatting.eslint_d,
-    null_ls.builtins.formatting.google_java_format,
-    null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.formatting.stylelint,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.jq,
+    formatting.eslint_d,
+    formatting.google_java_format,
+    formatting.prettierd,
+    formatting.shfmt,
+    formatting.stylelint,
+    formatting.stylua,
+    formatting.jq,
     hover.dictionary,
     linters.eslint_d.with({
       condition = function(utils)
@@ -54,19 +67,13 @@ null_ls.setup({
     linters.actionlint,
   },
   on_attach = function(client, bufnr)
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
-            filter = function(server)
-              return server.name ~= "tsserver"
-            end,
-            bufnr = bufnr,
-          })
+          lsp_formatting(bufnr)
         end,
       })
     end
