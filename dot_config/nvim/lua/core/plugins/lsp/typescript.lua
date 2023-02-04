9b99b5773361f59119ce6cd48e8dfd7eaa8725e6
Niels Mentink <niels@ment.ink>
2023-02-04 17:43:46 +0100

    Update .config/nvim/lua/core/plugins/lsp/typescript.lua

--- dot_config/nvim/lua/core/plugins/lsp/typescript.lua
+++ dot_config/nvim/lua/core/plugins/lsp/typescript.lua
@@ -4,17 +4,31 @@ local i = require("config.icons")
 local M = {
   {
     "marilari88/twoslash-queries.nvim",
-    init = function()
+    config = function()
       local tsq = require("twoslash-queries")
 
       tsq.setup({})
 
       require("core.util").on_attach(tsq.attach)
+
+      require("core.util").on_attach(function(client, buffer)
+        if client.name ~= "tsserver" then
+          vim.keymap.set(
+            "n",
+            "<leader>lq",
+            "<cmd>InspectTwoslashQueries<cr>",
+            { desc = i.TypeParameter .. "Inspect type", buffer = buffer }
+          )
+
+          vim.keymap.set(
+            "n",
+            "<leader>ld",
+            "<cmd>RemoveTwoslashQueries<cr>",
+            { desc = i.Error .. "Remove type inspect", buffer = buffer }
+          )
+        end
+      end)
     end,
-    keys = {
-      { "<leader>lq", "<cmd>InspectTwoslashQueries<cr>", desc = i.TypeParameter .. "Inspect type" },
-      { "<leader>ld", "<cmd>RemoveTwoslashQueries<cr>", desc = i.Error .. "Remove type inspect" },
-    },
   },
 
   {
