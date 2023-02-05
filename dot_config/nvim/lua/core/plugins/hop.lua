d81e2c6b4b3e6de8aa074340e2d177ae749bd73a
Niels Mentink <niels@ment.ink>
2023-02-05 23:07:47 +0100

    Update .config/nvim/lua/core/plugins/hop.lua

--- dot_config/nvim/lua/core/plugins/hop.lua
+++ dot_config/nvim/lua/core/plugins/hop.lua
@@ -6,7 +6,16 @@ local M = {
     {
       "f",
       function()
-        require("hop").hint_char2()
+        require("hop").hint_char1()
+      end,
+      remap = true,
+      desc = "Hop to char",
+      mode = { "n", "v" },
+    },
+    {
+      "F",
+      function()
+        require("hop").hint_words()
       end,
       remap = true,
       desc = "Hop to char",
