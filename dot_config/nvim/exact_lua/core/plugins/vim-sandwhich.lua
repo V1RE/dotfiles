40873bfa588a4c1d1d83e1ad1b4c58ecd8ec2f90
Niels Mentink <niels@ment.ink>
2023-02-07 10:45:09 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/vim-sandwhich.lua

--- dot_config/nvim/exact_lua/core/plugins/vim-sandwhich.lua
+++ dot_config/nvim/exact_lua/core/plugins/vim-sandwhich.lua
@@ -1,7 +1,43 @@
----@type LazyPluginSpec
+---@type LazyPluginSpec[]
 local M = {
-  "machakann/vim-sandwich",
-  event = "BufReadPost",
+  {
+    "machakann/vim-sandwich",
+    event = "BufReadPost",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      -- Populate the keys based on the user's options
+      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local mappings = {
+        { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "Delete surrounding" },
+        { opts.mappings.find, desc = "Find right surrounding" },
+        { opts.mappings.find_left, desc = "Find left surrounding" },
+        { opts.mappings.highlight, desc = "Highlight surrounding" },
+        { opts.mappings.replace, desc = "Replace surrounding" },
+        { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+      }
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = "gza", -- Add surrounding in Normal and Visual modes
+        delete = "gzd", -- Delete surrounding
+        find = "gzf", -- Find surrounding (to the right)
+        find_left = "gzF", -- Find surrounding (to the left)
+        highlight = "gzh", -- Highlight surrounding
+        replace = "gzr", -- Replace surrounding
+        update_n_lines = "gzn", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      -- use gz mappings instead of s to prevent conflict with leap
+      require("mini.surround").setup(opts)
+    end,
+  },
 }
 
 return M
