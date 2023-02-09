d7116fd7d67fb265e73492f7cc90f0997766bb52
Niels Mentink <niels@ment.ink>
2023-02-09 12:03:49 +0100

    Update .config/nvim/lua/core/plugins/lsp/dap.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/dap.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/dap.lua
@@ -10,6 +10,8 @@ local M = {
       end,
     },
     { "jbyuki/one-small-step-for-vimkind" },
+    { "mxsdev/nvim-dap-vscode-js"
+ },
   },
 }
 
@@ -65,6 +67,16 @@ function M.config()
   dap.adapters.nlua = function(callback, config)
     callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
   end
+
+  require("dap-vscode-js").setup({
+    -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+    -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+    -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+    -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+  })
 
   for _, language in ipairs({ "typescript", "javascript" }) do
     require("dap").configurations[language] = {
