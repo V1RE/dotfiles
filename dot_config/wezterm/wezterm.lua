95272a4db499a9464935223fa4f373176fac2165
Niels Mentink <niels@ment.ink>
2023-03-22 23:34:25 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -16,21 +16,14 @@ local function font_with_fallback(name, params)
 end
 
 local function isViProcess(pane)
-  wezterm.log_info(pane:get_foreground_process_info())
-  -- get_foreground_process_name On Linux, macOS and Windows,
-  -- the process can be queried to determine this path. Other operating systems
-  -- (notably, FreeBSD and other unix systems) are not currently supported
-  return pane:get_foreground_process_name():find("n?vim") ~= nil
-  -- return pane:get_title():find("n?vim") ~= nil
+  local name = "" .. pane:get_foreground_process_name()
+
+  return name:find("n?vim") ~= nil or name:find("chezmoi") ~= nil
 end
 
 local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
   if isViProcess(pane) then
-    window:perform_action(
-      -- This should match the keybinds you set in Neovim.
-      act.SendKey({ key = vim_direction, mods = "CTRL" }),
-      pane
-    )
+    window:perform_action(act.SendKey({ key = vim_direction, mods = "CTRL" }), pane)
   else
     window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
   end
