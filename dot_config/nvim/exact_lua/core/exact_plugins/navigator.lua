4ed1b2cf7f80190d4fd0000309ac62def2cc7dcc
Niels Mentink <niels@ment.ink>
2023-03-22 22:48:17 +0100

    Update .config/nvim/lua/core/plugins/navigator.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/navigator.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/navigator.lua
@@ -3,6 +3,13 @@ local M = {
   "numToStr/Navigator.nvim",
   event = "VeryLazy",
   config = true,
+  keys = {
+    { "<c-h>", "<CMD>NavigatorLeft<CR>", mode = { "n", "t" } },
+    { "<c-l>", "<CMD>NavigatorRight<CR>", mode = { "n", "t" } },
+    { "<c-k>", "<CMD>NavigatorUp<CR>", mode = { "n", "t" } },
+    { "<c-j>", "<CMD>NavigatorDown<CR>", mode = { "n", "t" } },
+    { "<c-p>", "<CMD>NavigatorPrevious<CR>", mode = { "n", "t" } },
+  },
 }
 
 return M
