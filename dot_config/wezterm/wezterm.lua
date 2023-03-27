e1dd014a53b38537708cf3efb3b524638bf435dd
Niels Mentink <niels@ment.ink>
2023-03-27 22:17:47 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -52,12 +52,6 @@ local old_config = {
   -- color_scheme_dirs = { "./colors/" },
   color_scheme = "Molokai",
   term = "wezterm",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
   window_background_opacity = 0.95,
   window_decorations = "RESIZE",
   hyperlink_rules = {
