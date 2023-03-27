bc6b1ada0107e71e0ac11750fd1eb9d7e1cc0c58
Niels Mentink <niels@ment.ink>
2023-03-27 23:21:24 +0200

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -35,10 +35,6 @@ config.window_frame = {
 
 config.pane_focus_follows_mouse = true
 
-config.hide_tab_bar_if_only_one_tab = false
-config.use_fancy_tab_bar = true
-config.tab_bar_at_bottom = false
-
 config.leader = {
   key = "b",
   mods = "CMD",
