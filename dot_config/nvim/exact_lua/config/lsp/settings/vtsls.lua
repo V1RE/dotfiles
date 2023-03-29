2a9d4317da4c2b51cabaf540cc3bed92d863abb2
Niels Mentink <niels@ment.ink>
2023-03-29 11:01:55 +0200

    Update .neoconf.json Update .config/nvim/lua/config/lsp/mason.lua Add .config/nvim/lua/config/lsp/settings/vtsls.lua

---
+++ dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
@@ -0,0 +1,29 @@
+---@type lspconfig.options.vtsls
+local tsserver = {
+  settings = {
+    javascript = { format = { enable = false } },
+    typescript = {
+      format = { enable = false },
+      implementationsCodeLens = { enabled = true },
+      inlayHints = {
+        enumMemberValues = { enabled = true },
+        parameterNames = { enabled = "all" },
+        functionLikeReturnTypes = { enabled = true },
+        parameterTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        variableTypes = { enabled = true },
+      },
+      preferences = {
+        useAliasesForRenames = false,
+        importModuleSpecifier = "non-relative",
+      },
+      experimental = {
+        enableProjectDiagnostics = true,
+        completion = { enableServerSideFuzzyMatch = true, entriesLimit = 25 },
+        tsserver = { maxTsServerMemory = 4096 },
+      },
+    },
+  },
+}
+
+return tsserver
