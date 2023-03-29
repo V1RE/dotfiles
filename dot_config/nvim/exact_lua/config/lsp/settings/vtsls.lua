0a79aed9b050c75d657a4c7a13e26a1113a6c162
Niels Mentink <niels@ment.ink>
2023-03-29 21:34:27 +0200

    Update .config/nvim/lua/config/lsp/settings/vtsls.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
@@ -1,26 +1,28 @@
 ---@type lspconfig.options.vtsls
 local tsserver = {
   settings = {
-    javascript = { format = { enable = false } },
-    typescript = {
-      format = { enable = false },
-      implementationsCodeLens = { enabled = true },
-      inlayHints = {
-        enumMemberValues = { enabled = true },
-        parameterNames = { enabled = "all" },
-        functionLikeReturnTypes = { enabled = true },
-        parameterTypes = { enabled = true },
-        propertyDeclarationTypes = { enabled = true },
-        variableTypes = { enabled = true },
-      },
-      preferences = {
-        useAliasesForRenames = false,
-        importModuleSpecifier = "non-relative",
-      },
-      experimental = {
-        enableProjectDiagnostics = true,
-        completion = { enableServerSideFuzzyMatch = true, entriesLimit = 25 },
-        tsserver = { maxTsServerMemory = 4096 },
+    vtsls = {
+      javascript = { format = { enable = false } },
+      typescript = {
+        format = { enable = false },
+        implementationsCodeLens = { enabled = true },
+        inlayHints = {
+          enumMemberValues = { enabled = true },
+          parameterNames = { enabled = "all" },
+          functionLikeReturnTypes = { enabled = true },
+          parameterTypes = { enabled = true },
+          propertyDeclarationTypes = { enabled = true },
+          variableTypes = { enabled = true },
+        },
+        preferences = {
+          useAliasesForRenames = false,
+          importModuleSpecifier = "non-relative",
+        },
+        experimental = {
+          enableProjectDiagnostics = true,
+          completion = { enableServerSideFuzzyMatch = true, entriesLimit = 25 },
+          tsserver = { maxTsServerMemory = 4096 },
+        },
       },
     },
   },
