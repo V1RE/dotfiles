dc4dcc0a965b3c44ba3c65bf9bf5f7dd9daf7a57
Niels Mentink <niels@ment.ink>
2023-04-12 22:18:27 +0200

    Update .config/nvim/lua/core/plugins/nvim-notify.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/nvim-notify.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/nvim-notify.lua
@@ -16,10 +16,10 @@ local M = {
     timeout = 3000,
     level = vim.log.levels.WARN,
     max_height = function()
-      return math.floor(vim.o.lines * 0.75)
+      return math.floor(vim.o.lines * 0.5)
     end,
     max_width = function()
-      return math.floor(vim.o.columns * 0.75)
+      return math.floor(vim.o.columns * 0.5)
     end,
   },
 }
