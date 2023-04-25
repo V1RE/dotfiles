213253385d58267f49672894b5a93c9864d9aa55
Niels Mentink <niels@ment.ink>
2023-04-25 13:10:44 +0200

    Update .config/nvim/lua/core/plugins/goto-preview.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/goto-preview.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/goto-preview.lua
@@ -3,6 +3,38 @@ local M = {
   {
     "rmagatti/goto-preview",
     setup = true,
+    keys = {
+      {
+        "gpd",
+        function()
+          require("goto-preview").goto_preview_definition()
+        end,
+      },
+      {
+        "gpt",
+        function()
+          require("goto-preview").goto_preview_type_definition()
+        end,
+      },
+      {
+        "gpi",
+        function()
+          require("goto-preview").goto_preview_implementation()
+        end,
+      },
+      {
+        "gP",
+        function()
+          require("goto-preview").close_all_win()
+        end,
+      },
+      {
+        "gpr",
+        function()
+          require("goto-preview").goto_preview_references()
+        end,
+      },
+    },
   },
 }
 
