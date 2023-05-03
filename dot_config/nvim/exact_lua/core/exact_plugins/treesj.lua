3dd016870e88865a2f07b5fe2d088daedd7a3784
Niels Mentink <niels@ment.ink>
2023-05-03 10:50:44 +0200

    Update .config/nvim/lua/core/plugins/treesj.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/treesj.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/treesj.lua
@@ -3,7 +3,11 @@ local M = {
   {
     "Wansmer/treesj",
     keys = {
-      { "<leader>J", "<cmd>TSJoin<cr>", desc = "Toggle SJ" },
+      {
+        "<leader>J",
+        "<cmd>TSToggle<cr>",
+        desc = "Toggle SJ",
+      },
     },
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = {
