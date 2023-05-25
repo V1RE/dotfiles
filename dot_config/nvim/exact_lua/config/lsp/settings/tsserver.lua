74e045d1c209a88533961d9977c3730c8360f840
Niels Mentink <niels@ment.ink>
2023-05-25 22:22:24 +0200

    Update .config/nvim/lua/config/lsp/settings/tsserver.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/tsserver.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/tsserver.lua
@@ -4,9 +4,41 @@ local util = require("lspconfig.util")
 local tsserver = {
   root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
   settings = {
-    javascript = { format = { enable = false } },
+    javascript = {
+      format = { enable = false },
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
     typescript = {
       format = { enable = false },
+      implementationsCodeLens = { enabled = true },
+      inlayHints = {
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+      preferences = {
+        useAliasesForRenames = false,
+        importModuleSpecifier = "non-relative",
+      },
+      experimental = {
+        enableProjectDiagnostics = true,
+        completion = { enableServerSideFuzzyMatch = true, entriesLimit = 25 },
+        tsserver = { maxTsServerMemory = 4096 },
+      },
     },
   },
 }
