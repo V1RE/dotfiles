83a216e660fc835513bb32eebe65ea8dad8876da
Niels Mentink <niels@ment.ink>
2023-05-26 09:36:50 +0200

    Update .config/nvim/lua/config/lsp/settings/lua_ls.lua Update .config/nvim/lua/core/plugins/lualine.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/lua_ls.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/lua_ls.lua
@@ -8,6 +8,7 @@ local lua_ls = {
       hint = {
         enable = true,
         setType = true,
+        arrayIndex = "Disable",
       },
       format = {
         enable = false,
--- dot_config/nvim/exact_lua/core/exact_plugins/lualine.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/lualine.lua
@@ -82,7 +82,17 @@ local M = {
       local progress = function()
         local current_line = vim.fn.line(".")
         local total_lines = vim.fn.line("$")
-        local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+        local chars = {
+          "__",
+          "▁▁",
+          "▂▂",
+          "▃▃",
+          "▄▄",
+          "▅▅",
+          "▆▆",
+          "▇▇",
+          "██",
+        }
         local line_ratio = current_line / total_lines
         local index = math.ceil(line_ratio * #chars)
         return chars[index]
