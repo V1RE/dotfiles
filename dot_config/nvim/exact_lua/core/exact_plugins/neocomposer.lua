c395190c65203be9a2173ac7ff2387eee994edcc
Niels Mentink <niels@ment.ink>
2023-05-30 13:16:11 +0200

    Update .config/nvim/lua/core/plugins/neocomposer.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/neocomposer.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/neocomposer.lua
@@ -1,36 +1,36 @@
--- ---@class NeoComposer
---
--- ---@class NeoComposer.Colors
--- ---@field bg string
--- ---@field fg string
--- ---@field red string
---     blue = "#5fb3b3",
---     green = "#99c794",
---
--- ---@class NeoComposer.Config
--- ---@field notify boolean
--- ---@field delay_timer number
--- ---@field colors table<string, string>
--- local config = {
---   notify = true,
---   delay_timer = 150,
---   colors = {
---     bg = "#16161e",
---     fg = "#ff9e64",
---     red = "#ec5f67",
---     blue = "#5fb3b3",
---     green = "#99c794",
---   },
---   keymaps = {
---     play_macro = "Q",
---     yank_macro = "yq",
---     stop_macro = "cq",
---     toggle_record = "q",
---     cycle_next = "<c-n>",
---     cycle_prev = "<c-p>",
---     toggle_macro_menu = "<m-q>",
---   },
--- }
+---@class NeoComposer
+
+---@class NeoComposer.Colors
+---@field bg string
+---@field fg string
+---@field red string
+---@field blue string
+---@field green string
+
+---@class NeoComposer.Config
+---@field notify boolean
+---@field delay_timer number
+---@field colors table<string, string>
+local config = {
+  notify = true,
+  delay_timer = 150,
+  colors = {
+    bg = "#16161e",
+    fg = "#ff9e64",
+    red = "#ec5f67",
+    blue = "#5fb3b3",
+    green = "#99c794",
+  },
+  keymaps = {
+    play_macro = "Q",
+    yank_macro = "yq",
+    stop_macro = "cq",
+    toggle_record = "q",
+    cycle_next = "<c-n>",
+    cycle_prev = "<c-p>",
+    toggle_macro_menu = "<m-q>",
+  },
+}
 
 ---@type LazyPluginSpec[]
 local M = {
