ffd339c2c9f8babf0e8fcc4bd3418d7a0c146494
Niels Mentink <niels@ment.ink>
2023-06-05 10:44:48 +0200

    Add .config/nvim/lua/core/plugins/sentiment.lua

---
+++ dot_config/nvim/exact_lua/core/exact_plugins/sentiment.lua
@@ -0,0 +1,16 @@
+---@type LazyPluginSpec[]
+local M = {
+  {
+    "utilyre/sentiment.nvim",
+    version = "*",
+    event = "VeryLazy", -- keep for lazy loading
+    opts = {
+      -- config
+    },
+    init = function()
+      vim.g.loaded_matchparen = 1
+    end,
+  },
+}
+
+return M
