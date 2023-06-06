e2a61666450aad3b4d4a25833c5ff117e79ddd3e
Niels Mentink <niels@ment.ink>
2023-06-06 12:31:39 +0200

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/trouble.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/trouble.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/trouble.lua
@@ -3,6 +3,14 @@ local M = {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
+  keys = {
+    { "<leader>xx", "<cmd>TroubleToggle<cr>", silent = true, noremap = true },
+    { "<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", silent = true, noremap = true },
+    { "<leader>xd", "<cmd>TroubleToggle lsp_document_diagnostics<cr>", silent = true, noremap = true },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", silent = true, noremap = true },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", silent = true, noremap = true },
+    { "gR", "<cmd>TroubleToggle lsp_references<cr>", silent = true, noremap = true },
+  },
 }
 
 return M
