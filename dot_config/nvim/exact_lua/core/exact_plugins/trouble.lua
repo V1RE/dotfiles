1138f485131169c7544a0b2a0bd894c1e1e07f78
Niels Mentink <niels@ment.ink>
2023-06-06 12:33:14 +0200

    Update .config/nvim/lua/core/plugins/trouble.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/trouble.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/trouble.lua
@@ -1,7 +1,6 @@
 ---@type LazyPluginSpec
 local M = {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
   keys = {
     { "<leader>xx", "<cmd>TroubleToggle<cr>", silent = true, noremap = true },
