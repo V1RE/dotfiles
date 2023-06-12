89e6538a93e3ed5fda21fdc5c6c3238530ff43df
Niels Mentink <niels@ment.ink>
2023-06-12 15:16:24 +0200

    Update .config/nvim/lua/core/plugins/lsp/java.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/java.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/java.lua
@@ -1,90 +1,89 @@
-local cache_vars = {}
-
-local root_files = {
-  ".git",
-  "mvnw",
-  "gradlew",
-  "pom.xml",
-  "build.gradle",
-}
-
-local function get_jdtls_paths()
-  if cache_vars.paths then
-    return cache_vars.paths
-  end
-
-  local path = {}
-
-  path.data_dir = vim.fn.stdpath("cache") .. "/nvim-jdtls"
-
-  local jdtls_install = require("mason-registry").get_package("jdtls"):get_install_path()
-
-  path.java_agent = jdtls_install .. "/lombok.jar"
-  path.launcher_jar = vim.fn.glob(jdtls_install .. "/plugins/org.eclipse.equinox.launcher_*.jar")
-
-  if vim.fn.has("mac") == 1 then
-    path.platform_config = jdtls_install .. "/config_mac"
-  elseif vim.fn.has("unix") == 1 then
-    path.platform_config = jdtls_install .. "/config_linux"
-  elseif vim.fn.has("win32") == 1 then
-    path.platform_config = jdtls_install .. "/config_win"
-  end
-
-  path.bundles = {}
-
-  ---
-  -- Include java-test bundle if present
-  ---
-  local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
-
-  local java_test_bundle = vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar"), "\n")
-
-  if java_test_bundle[1] ~= "" then
-    vim.list_extend(path.bundles, java_test_bundle)
-  end
-
-  ---
-  -- Include java-debug-adapter bundle if present
-  ---
-  local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
-
-  local java_debug_bundle =
-    vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n")
-
-  if java_debug_bundle[1] ~= "" then
-    vim.list_extend(path.bundles, java_debug_bundle)
-  end
-
-  ---
-  -- Useful if you're starting jdtls with a Java version that's
-  -- different from the one the project uses.
-  ---
-  path.runtimes = {
-    -- Note: the field `name` must be a valid `ExecutionEnvironment`,
-    -- you can find the list here:
-    -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-    --
-    -- This example assume you are using sdkman: https://sdkman.io
-    -- {
-    --   name = 'JavaSE-17',
-    --   path = vim.fn.expand('~/.sdkman/candidates/java/17.0.6-tem'),
-    -- },
-    -- {
-    --   name = 'JavaSE-18',
-    --   path = vim.fn.expand('~/.sdkman/candidates/java/18.0.2-amzn'),
-    -- },
-  }
-
-  cache_vars.paths = path
-
-  return path
-end
-
 ---@type LazyPluginSpec[]
 local M = {
   {
     "mfussenegger/nvim-jdtls",
     config = function()
+      local cache_vars = {}
+
+      local root_files = {
+        ".git",
+        "mvnw",
+        "gradlew",
+        "pom.xml",
+        "build.gradle",
+      }
+
+      local function get_jdtls_paths()
+        if cache_vars.paths then
+          return cache_vars.paths
+        end
+
+        local path = {}
+
+        path.data_dir = vim.fn.stdpath("cache") .. "/nvim-jdtls"
+
+        local jdtls_install = require("mason-registry").get_package("jdtls"):get_install_path()
+
+        path.java_agent = jdtls_install .. "/lombok.jar"
+        path.launcher_jar = vim.fn.glob(jdtls_install .. "/plugins/org.eclipse.equinox.launcher_*.jar")
+
+        if vim.fn.has("mac") == 1 then
+          path.platform_config = jdtls_install .. "/config_mac"
+        elseif vim.fn.has("unix") == 1 then
+          path.platform_config = jdtls_install .. "/config_linux"
+        elseif vim.fn.has("win32") == 1 then
+          path.platform_config = jdtls_install .. "/config_win"
+        end
+
+        path.bundles = {}
+
+        ---
+        -- Include java-test bundle if present
+        ---
+        local java_test_path = require("mason-registry").get_package("java-test"):get_install_path()
+
+        local java_test_bundle = vim.split(vim.fn.glob(java_test_path .. "/extension/server/*.jar"), "\n")
+
+        if java_test_bundle[1] ~= "" then
+          vim.list_extend(path.bundles, java_test_bundle)
+        end
+
+        ---
+        -- Include java-debug-adapter bundle if present
+        ---
+        local java_debug_path = require("mason-registry").get_package("java-debug-adapter"):get_install_path()
+
+        local java_debug_bundle =
+          vim.split(vim.fn.glob(java_debug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar"), "\n")
+
+        if java_debug_bundle[1] ~= "" then
+          vim.list_extend(path.bundles, java_debug_bundle)
+        end
+
+        ---
+        -- Useful if you're starting jdtls with a Java version that's
+        -- different from the one the project uses.
+        ---
+        path.runtimes = {
+          -- Note: the field `name` must be a valid `ExecutionEnvironment`,
+          -- you can find the list here:
+          -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+          --
+          -- This example assume you are using sdkman: https://sdkman.io
+          -- {
+          --   name = 'JavaSE-17',
+          --   path = vim.fn.expand('~/.sdkman/candidates/java/17.0.6-tem'),
+          -- },
+          -- {
+          --   name = 'JavaSE-18',
+          --   path = vim.fn.expand('~/.sdkman/candidates/java/18.0.2-amzn'),
+          -- },
+        }
+
+        cache_vars.paths = path
+
+        return path
+      end
       local jdtls = require("jdtls")
 
       local path = get_jdtls_paths()
