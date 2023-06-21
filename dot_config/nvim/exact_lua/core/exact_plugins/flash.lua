8bcfc0adb1488a9f79620c0652c5025daf64ee78
Niels Mentink <niels@ment.ink>
2023-06-21 21:32:30 +0200

    Add .config/nvim/lua/core/plugins/flash.lua

---
+++ dot_config/nvim/exact_lua/core/exact_plugins/flash.lua
@@ -0,0 +1,26 @@
+---@type LazyPluginSpec
+local M = {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        -- default options: exact mode, multi window, all directions, with a backdrop
+        require("flash").jump()
+      end,
+    },
+    {
+      "S",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+    },
+  },
+}
+
+return M
