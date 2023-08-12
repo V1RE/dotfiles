1caba587ce55800be3ea967d700b37cea88dfc0d
Niels Mentink <niels@ment.ink>
2023-08-13 00:57:16 +0200

    Update .config/nvim/lua/core/plugins/lsp/init.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/init.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/init.lua
@@ -1,6 +1,7 @@
 ---@type LazySpec[]
 local M = {
   { import = "core.plugins.lsp.typescript", ft = { "typescript", "typescriptreact" } },
+  { import = "core.plugins.lsp.go", ft = { "go", "gomod", "gosum" } },
   { import = "core.plugins.lsp.java", ft = { "java" }, enabled = false },
   { import = "core.plugins.lsp.dap" },
 }
