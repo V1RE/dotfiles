07599fc5b0cc8035aff0ecf84abf4900f8f2cb11
Niels Mentink <niels@ment.ink>
2023-10-07 18:17:50 +0200

    Update .config/nvim/lua/config/lsp/settings/jsonls.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/jsonls.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/jsonls.lua
@@ -7,7 +7,16 @@ local jsonls = {
   },
   settings = {
     json = {
-      schemas = schemastore.json.schemas(),
+      schemas = schemastore.json.schemas({
+        extra = {
+          {
+            url = "https://unpkg.com/syncpack/dist/schema.json",
+            fileMatch = ".syncpackrc.json",
+            name = "Syncpack",
+            description = "Consistent dependency versions in large JavaScript Monorepos",
+          },
+        },
+      }),
     },
   },
 }
