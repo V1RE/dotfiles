0ae934eb700239fb7336b0a6459fd972312a2ef9
Niels Mentink <niels@ment.ink>
2023-10-09 22:47:25 +0200

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/conform.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/conform.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/conform.lua
@@ -1,7 +1,21 @@
+local typescript = {"eslint_d",{"prettierd","prettier"}}
+
 ---@type LazyPluginSpec
 local M = {
   'stevearc/conform.nvim',
-  opts = {},
+  opts = {
+    ---@type table<string, conform.FormatterUnit[]>
+    formatters_by_ft = {
+      javascript = typescript,
+      typescript = typescript,
+      typescriptreact = typescript,
+      javascriptreact = typescript,
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+  },
 }
 
 return M
