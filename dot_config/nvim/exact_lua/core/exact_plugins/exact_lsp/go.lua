a0fd56e7e6e8178c178d6897aa78c4e80bf86853
Niels Mentink <niels@ment.ink>
2023-11-27 21:36:43 +0100

    Update .config/nvim/lua/core/plugins/lsp/go.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/go.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/go.lua
@@ -12,6 +12,7 @@ local M = {
   event = { "CmdlineEnter" },
   ft = { "go", "gomod" },
   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  enabled = false,
 }
 
 return M
