0cf6d293da1f65f386c1c44f0ff6d77f5ea7d1a0
Niels Mentink <niels@ment.ink>
2024-02-08 20:12:52 +0100

    Add .config/nvim/lua/core/plugins/codium.lua

---
+++ dot_config/nvim/exact_lua/core/exact_plugins/codium.lua
@@ -0,0 +1,11 @@
+---@type LazyPluginSpec
+local M = {
+  "Exafunction/codeium.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp",
+  },
+  config = true,
+}
+
+return M
