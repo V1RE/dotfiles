ae2030c718cfb6ddf3deb6f12263b366402c46cb
Niels Mentink <niels@ment.ink>
2024-02-13 12:22:03 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/config/copilot_rc.lua

--- dot_config/nvim/exact_lua/config/copilot_rc.lua
+++ dot_config/nvim/exact_lua/config/copilot_rc.lua
@@ -3,7 +3,7 @@ local copilot = require("copilot")
 vim.defer_fn(function()
   ---@type copilot_config
   local opts = {
-    copilot_node_command = "/usr/local/bin/node",
+    copilot_node_command = "/Users/niels/.local/share/mise/installs/bun/latest/bin/bun",
     ---@type copilot_config_panel
     panel = { enabled = false },
     suggestion = { enabled = false },
