9c0123e0175f19e219680e1bc02211a377a0a79c
Niels Mentink <niels@ment.ink>
2024-03-03 17:18:37 +0100

    Update .config/nvim/lua/core/plugins/lua-snip.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/lua-snip.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/lua-snip.lua
@@ -6,26 +6,20 @@ local M = {
     "rafamadriz/friendly-snippets",
   },
   config = function(_, opts)
-    if opts then
-      require("luasnip").config.setup(opts)
-    end
+    local ls = require("luasnip")
+
+    ls.config.setup(opts)
+
     vim.tbl_map(function(type)
       require("luasnip.loaders.from_" .. type).lazy_load()
     end, { "vscode", "snipmate", "lua" })
+
     -- friendly-snippets - enable standardized comments snippets
-    require("luasnip").filetype_extend("typescript", { "tsdoc" })
-    require("luasnip").filetype_extend("javascript", { "jsdoc" })
-    require("luasnip").filetype_extend("lua", { "luadoc" })
-    require("luasnip").filetype_extend("python", { "pydoc" })
-    require("luasnip").filetype_extend("rust", { "rustdoc" })
-    require("luasnip").filetype_extend("cs", { "csharpdoc" })
-    require("luasnip").filetype_extend("java", { "javadoc" })
-    require("luasnip").filetype_extend("c", { "cdoc" })
-    require("luasnip").filetype_extend("cpp", { "cppdoc" })
-    require("luasnip").filetype_extend("php", { "phpdoc" })
-    require("luasnip").filetype_extend("kotlin", { "kdoc" })
-    require("luasnip").filetype_extend("ruby", { "rdoc" })
-    require("luasnip").filetype_extend("sh", { "shelldoc" })
+    ls.filetype_extend("typescript", { "tsdoc" })
+    ls.filetype_extend("javascript", { "jsdoc" })
+    ls.filetype_extend("lua", { "luadoc" })
+    ls.filetype_extend("rust", { "rustdoc" })
+    ls.filetype_extend("sh", { "shelldoc" })
   end,
 }
 
