3eda8166526b135e8d96abf7bc3874025c307b28
Niels Mentink <niels@ment.ink>
2024-03-14 23:31:40 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/chezmoi.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/chezmoi.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/chezmoi.lua
@@ -16,6 +16,20 @@ local M = {
       return nil ~= vim.api.nvim_exec("args", true):find("chezmoi")
     end,
   },
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function(_, opts)
+      require("chezmoi").setup(opts)
+
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function()
+          vim.schedule(require("chezmoi.commands.__edit").watch)
+        end,
+      })
+    end,
+  },
 }
 
 return M
