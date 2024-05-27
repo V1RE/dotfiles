cb90d643e9ce680ed532cac4618e7c750837258b
Niels Mentink <niels@ment.ink>
2024-05-27 15:34:30 +0200

    Update .config/nvim/lua/config/lsp/mason.lua Update .config/nvim/lua/config/lsp/settings/vtsls.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
@@ -57,23 +57,6 @@ local tsserver = {
       },
     },
   },
-  before_init = function(params, config)
-    local result = vim
-      .system({ "pnpm", "why", "vue", "--depth=0" }, { cwd = params.workspaceFolders[1].name, text = true })
-      :wait()
-    if result.stdout ~= "[]" then
-      local vuePluginConfig = {
-        name = "@vue/typescript-plugin",
-        location = require("mason-registry").get_package("vue-language-server"):get_install_path()
-          .. "/node_modules/@vue/language-server",
-        languages = { "vue" },
-        configNamespace = "typescript",
-        enableForWorkspaceTypeScriptVersions = true,
-      }
-      table.insert(config.settings.vtsls.tsserver.globalPlugins, vuePluginConfig)
-    end
-  end,
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 }
 
 return tsserver
