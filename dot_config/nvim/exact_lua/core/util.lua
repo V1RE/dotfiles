588b64fb2b1a5c95efd56ac707190130b328fdf7
Niels Mentink <niels@ment.ink>
2024-06-05 09:21:12 +0200

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/util.lua

--- dot_config/nvim/exact_lua/core/util.lua
+++ dot_config/nvim/exact_lua/core/util.lua
@@ -71,41 +71,4 @@ function M.float_term(cmd, opts)
   require("lazy.util").float_term(cmd, opts)
 end
 
----@param silent boolean?
----@param values? {[1]:any, [2]:any}
-function M.toggle(option, silent, values)
-  if values then
-    if vim.opt_local[option]:get() == values[1] then
-      vim.opt_local[option] = values[2]
-    else
-      vim.opt_local[option] = values[1]
-    end
-    return vim.notify(
-      "Set " .. option .. " to " .. vim.opt_local[option]:get(),
-      vim.log.levels.INFO,
-      { title = "Option" }
-    )
-  end
-  vim.opt_local[option] = not vim.opt_local[option]:get()
-  if not silent then
-    vim.notify(
-      (vim.opt_local[option]:get() and "Enabled" or "Disabled") .. " " .. option,
-      vim.log.levels.INFO,
-      { title = "Option" }
-    )
-  end
-end
-
-local enabled = true
-function M.toggle_diagnostics()
-  enabled = not enabled
-  if enabled then
-    vim.diagnostic.enable()
-    vim.notify("Enabled diagnostics", vim.log.levels.INFO, { title = "Diagnostics" })
-  else
-    vim.diagnostic.disable()
-    vim.notify("Disabled diagnostics", vim.log.levels.INFO, { title = "Diagnostics" })
-  end
-end
-
 return M
