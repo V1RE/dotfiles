8fc67039218e2507e397ddf8bd3afd29dd500e86
Niels Mentink <niels@ment.ink>
2024-06-07 09:42:48 +0200

    Update .config/nvim/lua/config/lsp/settings/eslint.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/eslint.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/eslint.lua
@@ -1,5 +1,11 @@
 ---@type lspconfig.options.eslint
 local eslint = {
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
   settings = {
     eslint = {
       autoFixOnSave = true,
