17a7cef650ce9f0bf02e5d89c872b087be9fa17a
Niels Mentink <niels@ment.ink>
2024-07-05 13:32:59 +0200

    Update .config/nvim/lua/core/util.lua

--- dot_config/nvim/exact_lua/core/util.lua
+++ dot_config/nvim/exact_lua/core/util.lua
@@ -26,14 +26,14 @@ function M.get_root()
   ---@type string[]
   local roots = {}
   if path then
-    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
       local workspace = client.config.workspace_folders
       local paths = workspace and vim.tbl_map(function(ws)
         return vim.uri_to_fname(ws.uri)
       end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
       for _, p in ipairs(paths) do
         local r = vim.loop.fs_realpath(p)
-        if path:find(r, 1, true) then
+        if r and path:find(r, 1, true) then
           roots[#roots + 1] = r
         end
       end
