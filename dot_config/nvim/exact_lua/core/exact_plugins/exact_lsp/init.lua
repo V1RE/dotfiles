9b14192a1c32db00dd9ad72427117d8ad7b4ee10
Niels Mentink <niels@ment.ink>
2024-07-07 22:37:31 +0200

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/lsp/init.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/init.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/exact_lsp/init.lua
@@ -4,14 +4,6 @@ local M = {
   { import = "core.plugins.lsp.go", ft = { "go", "gomod", "gosum" }, enabled = false },
   { import = "core.plugins.lsp.java", ft = { "java" }, enabled = false },
   { import = "core.plugins.lsp.dap", enabled = false },
-  {
-    "MysticalDevil/inlay-hints.nvim",
-    event = "LspAttach",
-    dependencies = { "neovim/nvim-lspconfig" },
-    config = function()
-      require("inlay-hints").setup()
-    end,
-  },
 }
 
 return M
