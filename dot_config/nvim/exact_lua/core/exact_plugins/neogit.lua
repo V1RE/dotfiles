d6ba3698712734d064b19cb0666273dd79d1cc34
Niels Mentink <niels@ment.ink>
2024-07-15 22:07:34 +0200

    Update .config/nvim/.lazy-lock.json Add .config/nvim/lua/core/plugins/neogit.lua

---
+++ dot_config/nvim/exact_lua/core/exact_plugins/neogit.lua
@@ -0,0 +1,12 @@
+---@type LazyPluginSpec
+local M = {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = true,
+}
+
+return M
