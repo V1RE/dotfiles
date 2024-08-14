718add884bb8b3a458d36cdebc548528f8af753f
Niels Mentink <niels@ment.ink>
2024-08-14 11:42:43 +0200

    Update .config/nvim/lua/core/plugins/mini-ai.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/mini-ai.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/mini-ai.lua
@@ -1,6 +1,13 @@
 ---@type LazyPluginSpec[]
 local M = {
-  { "echasnovski/mini.ai", version = false },
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+    end,
+  },
 }
 
 return M
