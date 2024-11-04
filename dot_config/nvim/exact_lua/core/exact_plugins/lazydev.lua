4fbe7b4309c1039552706a7809b4419f87b27059
Niels Mentink <niels@ment.ink>
2024-11-05 00:31:05 +0100

    Update .config/nvim/lua/core/plugins/lazydev.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
@@ -1,14 +1,16 @@
 ---@type LazyPluginSpec
 local M = {
-  "folke/lazydev.nvim",
-  ft = "lua",
-  opts = {
-    library = {
-      "lazy.nvim",
-      -- See the configuration section for more details
-      -- Load luvit types when the `vim.uv` word is found
-      { path = "luvit-meta/library", words = { "vim%.uv" } },
-      { path = "wezterm-types", mods = { "wezterm" } },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        "lazy.nvim",
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
+      },
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
