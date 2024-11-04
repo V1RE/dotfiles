5171f17a9a798e4a53d6f9979b979d8e5c019679
Niels Mentink <niels@ment.ink>
2024-11-05 00:24:56 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/lazydev.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
@@ -7,6 +7,7 @@ local M = {
       -- See the configuration section for more details
       -- Load luvit types when the `vim.uv` word is found
       { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "wezterm-types", mods = { "wezterm" } },
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
