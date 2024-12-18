8f008ab7705cbf73c11f49249232599b70b2ed47
Niels Mentink <niels@ment.ink>
2024-12-18 15:51:18 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/config/lsp/handlers.lua Update .config/nvim/lua/core/plugins/completion.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/completion.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/completion.lua
@@ -1,6 +1,51 @@
 ---@type LazyPluginSpec[]
 local M = {
   {
+    "saghen/blink.cmp",
+    lazy = false, -- lazy loading handled internally
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- see the "default configuration" section below for full documentation on how to define
+      -- your own keymap.
+      keymap = { preset = "enter" },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, via `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        -- optionally disable cmdline completions
+        -- cmdline = {},
+      },
+
+      -- experimental signature help support
+      signature = { enabled = true },
+    },
+    -- allows extending the providers array elsewhere in your config
+    -- without having to redefine it
+    opts_extend = { "sources.default" },
+  },
+}
+
+---@type LazyPluginSpec[]
+---@diagnostic disable-next-line: unused-local
+local oldCmp = {
+  {
     "hrsh7th/nvim-cmp",
     version = false, -- last release is way too old
     event = "InsertEnter",
