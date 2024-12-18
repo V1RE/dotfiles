af33c7cb74da0d2de7b4926150ec54c4e3938942
Niels Mentink <niels@ment.ink>
2024-12-18 15:54:49 +0100

    Update .config/nvim/lua/core/plugins/completion.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/completion.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/completion.lua
@@ -13,7 +13,9 @@ local M = {
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- see the "default configuration" section below for full documentation on how to define
       -- your own keymap.
-      keymap = { preset = "enter" },
+      keymap = {
+        preset = "default",
+      },
 
       appearance = {
         -- Sets the fallback highlight groups to nvim-cmp's highlight groups
