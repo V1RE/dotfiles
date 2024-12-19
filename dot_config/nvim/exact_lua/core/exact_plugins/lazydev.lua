510998ca0b1fd827257ff5133d73816554e76a30
Niels Mentink <niels@ment.ink>
2024-12-19 10:26:18 +0100

    Update .config/nvim/lua/core/plugins/lazydev.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/lazydev.lua
@@ -15,15 +15,21 @@ local M = {
     },
   },
   { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional cmp completion source for require statements and module annotations
-    "nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
+  { -- optional blink completion source for require statements and module annotations
+    "saghen/blink.cmp",
+    opts = {
+      sources = {
+        -- add lazydev to your completion providers
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+        },
+        providers = {
+          -- dont show LuaLS require statements when lazydev has items
+          lsp = { fallback_for = { "lazydev" } },
+          lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+        },
+      },
+    },
   },
 }
 
