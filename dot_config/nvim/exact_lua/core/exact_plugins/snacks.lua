a21be6424fa2f7648a97db8210cac66be770750a
Niels Mentink <niels@ment.ink>
2025-01-26 16:32:56 +0100

    Update .config/nvim/lua/core/plugins/snacks.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/snacks.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/snacks.lua
@@ -14,13 +14,6 @@ local M = {
         enabled = true,
         sections = {
           { section = "header" },
-          {
-            pane = 2,
-            section = "terminal",
-            cmd = "colorscript -e square",
-            height = 5,
-            padding = 1,
-          },
           { section = "keys", gap = 1, padding = 1 },
           {
             pane = 2,
