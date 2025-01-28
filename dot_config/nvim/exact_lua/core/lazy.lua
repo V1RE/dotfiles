0627a9780feb71e0da1fe5b150b84da3d3a56a27
Niels Mentink <niels@ment.ink>
2025-01-28 10:59:00 +0100

    Update .config/nvim/lua/core/lazy.lua

--- dot_config/nvim/exact_lua/core/lazy.lua
+++ dot_config/nvim/exact_lua/core/lazy.lua
@@ -11,21 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  spec = {
-    {
-      "LazyVim/LazyVim",
-      import = "lazyvim.plugins",
-      opts = {
-        -- colorscheme = "rose-pine",
-        news = {
-          lazyvim = true,
-          neovim = true,
-        },
-      },
-    },
-    { import = "core.plugins" },
-  },
+require("lazy").setup("core.plugins", {
   performance = {
     rtp = {
       disabled_plugins = {
