7c36d805446b8ad564083e59b0efb93670ffaed4
Niels Mentink <niels@ment.ink>
2025-01-28 10:49:34 +0100

    Update .config/nvim/lua/core/lazy.lua

--- dot_config/nvim/exact_lua/core/lazy.lua
+++ dot_config/nvim/exact_lua/core/lazy.lua
@@ -1,29 +1,43 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("core.plugins", {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        -- "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        -- "tarPlugin",
-        -- "tohtml",
-        -- "tutor",
-        -- "zipPlugin",
-      },
-    },
-  },
+	spec = {
+		{
+			"LazyVim/LazyVim",
+			import = "lazyvim.plugins",
+			opts = {
+				-- colorscheme = "rose-pine",
+				news = {
+					lazyvim = true,
+					neovim = true,
+				},
+			},
+		},
+		{ import = "plugins" },
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				-- "gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				-- "tarPlugin",
+				-- "tohtml",
+				-- "tutor",
+				-- "zipPlugin",
+			},
+		},
+	},
 })
