6b8bdb2fb41302cbd779f46156ab9bb30c020a7d
Niels Mentink <niels@ment.ink>
2025-01-31 02:15:40 +0100

    Update .config/nvim/lua/config/lsp/init.lua

--- dot_config/nvim/exact_lua/config/lsp/init.lua
+++ dot_config/nvim/exact_lua/config/lsp/init.lua
@@ -1,3 +1,12 @@
 require("config.lsp.handlers").setup()
 
 require("config.lsp.mason")
+
+vim.filetype.add({
+  extension = {
+    ["plist"] = "xml",
+  },
+  filename = {
+    ["bun.lock"] = "jsonc",
+  },
+})
