4503098c2ecd10971331837c9cd3fc947ae2e2bf
Niels Mentink <niels@ment.ink>
2025-01-31 02:20:03 +0100

    Update .config/nvim/lua/config/lsp/init.lua

--- dot_config/nvim/exact_lua/config/lsp/init.lua
+++ dot_config/nvim/exact_lua/config/lsp/init.lua
@@ -8,5 +8,17 @@ vim.filetype.add({
   },
   filename = {
     ["bun.lock"] = "jsonc",
+    ["*.env.*"] = "sh",
+    ["Podfile"] = "ruby",
+    ["Appfile"] = "ruby",
+    ["Fastfile"] = "ruby",
+    ["Matchfile"] = "ruby",
+    ["Pluginfile"] = "ruby",
+    ["*.ejs.t"] = "embedded_template",
+    ["*.hbs"] = "html",
+    ["*.liquid"] = "liquid.html",
+    [".prettierignore"] = "gitignore",
+    ["dot_zshrc"] = "zsh",
+    ["dot_zshenv"] = "zsh",
   },
 })
