3861553217c7e4a00ecccfdc3950101c74cd9c5e
Niels Mentink <niels@ment.ink>
2025-02-04 16:34:45 +0100

    Update .config/nvim/.lazy-lock.json Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -6,8 +6,27 @@ local function centerMouse()
   hs.mouse.absolutePosition(center)
 end
 
+local logger = hs.logger.new("init.lua", "debug")
+
+local function devTools()
+  local app = hs.application.find("com.google.Chrome.canary")
+  local window = app:findWindow("- react native devtools")
+
+  logger:d(app, window)
+
+  if app:isFrontmost() then
+    app:hide()
+  else
+    window:focus()
+  end
+end
+
 hs.hotkey.bind({ "cmd", "ctrl" }, "j", function()
   centerMouse()
   hs.eventtap.keyStroke("cmd", "tab")
   hs.eventtap.keyStroke({})
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "n", function()
+  devTools()
 end)
