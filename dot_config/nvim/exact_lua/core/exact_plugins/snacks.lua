3ef3bf6c0d900964929942c60f20434db7bb00f0
Niels Mentink <niels@ment.ink>
2025-02-06 00:42:56 +0100

    Update .config/nvim/lua/core/plugins/snacks.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/snacks.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/snacks.lua
@@ -21,6 +21,83 @@ local M = {
       lazygit = {},
       picker = {
         ui_select = true,
+        previewers = {
+          files = {
+
+            ---@param ctx snacks.picker.preview.ctx
+            preview = function(ctx)
+              local check_is_image = function(fileName)
+                local image_extensions = { "png", "jpg", "webp" } -- Supported image formats
+                local split_path = vim.split(fileName:lower(), ".", { plain = true })
+                local extension = split_path[#split_path]
+                return vim.tbl_contains(image_extensions, extension)
+              end
+
+              local fileName = ctx.item.file
+
+              local bufnr = ctx.preview.win.buf
+              local cwd = ctx.item.cwd
+
+              -- Clear previous image
+              if ctx.prev ~= nil then
+                local prev_image = images[require("plenary.path"):new(ctx.prev.cwd):joinpath(ctx.prev.file):absolute()]
+
+                if prev_image then
+                  prev_image:clear(true)
+                end
+              end
+
+              -- Fallback to the default implementation if the file is not an image or we don't have image.nvim
+              if not check_is_image(fileName) or not pcall(require, "image") then
+                return require("snacks").picker.preview.file(ctx)
+              end
+
+              -- Remove the content from the previous preview
+              ctx.preview:set_lines({})
+
+              local filepath = require("plenary.path"):new(cwd):joinpath(fileName):absolute()
+              local autocmd
+
+              -- Clear used memory and the images array when the picker is closed
+              autocmd = vim.api.nvim_create_autocmd("WinClosed", {
+                callback = function(evt)
+                  if ctx.picker.closed then
+                    for _, i in ipairs(images) do
+                      i:clear(false)
+                    end
+                    images = {}
+
+                    -- Remove the autocmd since we don't need it anymore
+                    if autocmd ~= nil then
+                      vim.api.nvim_del_autocmd(autocmd)
+                    end
+                  end
+                end,
+              })
+
+              local image = images[filepath]
+
+              if image then
+                image:render()
+                return
+              end
+
+              image = require("image").from_file(filepath, {
+                window = ctx.win,
+                buffer = bufnr,
+                width = vim.api.nvim_win_get_width(ctx.win),
+                with_virtual_padding = true,
+              })
+
+              images[filepath] = image
+
+              if not image then
+                return
+              end
+              image:render()
+            end,
+          },
+        },
       },
     },
     keys = {
@@ -128,15 +205,35 @@ local M = {
       },
     },
     init = function()
-      vim.api.nvim_create_user_command("Snacks", function(command)
-        Snacks.picker.pick(command.args or "files")
-      end, {
-        bar = true,
-        bang = true,
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "VeryLazy",
+        callback = function()
+          -- Setup some globals for debugging (lazy-loaded)
+          _G.dd = function(...)
+            Snacks.debug.inspect(...)
+          end
+          _G.bt = function()
+            Snacks.debug.backtrace()
+          end
+          vim.print = _G.dd -- Override print to use snacks for `:=` command
+          --
+          -- -- Create some toggle mappings
+          -- Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>os")
+          -- Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>ow")
+          -- Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>oL")
+          -- Snacks.toggle.diagnostics():map("<leader>od")
+          -- Snacks.toggle.line_number():map("<leader>ol")
+          -- Snacks.toggle
+          --   .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+          --   :map("<leader>oc")
+          -- Snacks.toggle.treesitter():map("<leader>oT")
+          -- Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ob")
+          -- Snacks.toggle.inlay_hints():map("<leader>oh")
+          -- Snacks.toggle.indent():map("<leader>og")
+          -- Snacks.toggle.dim():map("<leader>oD")
+        end,
       })
     end,
-
-    cmd = { "Snacks" },
   },
 }
 
