d05bf78a6fe6b76e7f28e07cf96d9991cf8e5f39
Niels Mentink <niels@ment.ink>
2025-09-30 09:48:25 +0200

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/legacy.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/legacy.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/legacy.lua
@@ -14,7 +14,7 @@ return {
   { "neovim/nvim-lspconfig", dependencies = { "mason-lspconfig.nvim" } },
   { "nvim-lua/plenary.nvim" },
   { "tpope/vim-abolish" },
-  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason.nvim", version = "2.0.1" },
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   { "justinsgithub/wezterm-types" },
 }
