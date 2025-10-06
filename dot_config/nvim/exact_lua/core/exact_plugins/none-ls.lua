32f19d4ad7492fa74f99dc1fbe4594f96516ed70
Niels Mentink <niels@ment.ink>
2025-10-06 09:59:11 +0200

    Update .config/nvim/lua/core/plugins/none-ls.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/none-ls.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/none-ls.lua
@@ -11,7 +11,7 @@ end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
+  if client:supports_method("textDocument/formatting") then
     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = augroup,
