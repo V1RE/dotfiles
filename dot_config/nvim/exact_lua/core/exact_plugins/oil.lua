8e1a264d27ef71b46f9b56986f0368565db10996
Niels Mentink <niels@ment.ink>
2026-01-06 10:39:03 +0100

    Update .config/nvim/lua/core/plugins/oil.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/oil.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/oil.lua
@@ -7,6 +7,23 @@ return {
   opts = {
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
+    keymaps = {
+      ["gy"] = {
+        function()
+          local dir = require("oil").get_current_dir()
+          vim.fn.setreg("+", dir)
+          vim.notify("Copied: " .. dir)
+        end,
+        desc = "Copy directory path",
+      },
+      ["go"] = {
+        function()
+          local dir = require("oil").get_current_dir()
+          vim.fn.system({ "open", dir })
+        end,
+        desc = "Open in Finder",
+      },
+    },
   },
   lazy = false,
   keys = {
