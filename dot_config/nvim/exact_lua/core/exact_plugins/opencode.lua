0cc278ad2b1b5b0913da2be65d3df48700f0f6a0
Niels Mentink <niels@ment.ink>
2026-03-18 01:41:31 +0100

    Update .config/nvim/.lazy-lock.json Update .config/nvim/lua/core/plugins/opencode.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/opencode.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/opencode.lua
@@ -13,14 +13,53 @@ return {
           percent = 35,
         },
       },
+      lsp = {
+        enabled = true,
+      },
     }
     vim.o.autoread = true
   end,
   keys = {
-    { "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, mode = { "n", "x" }, desc = "Ask opencode" },
-    { "<leader>os", function() require("opencode").select() end, mode = { "n", "x" }, desc = "Select opencode action" },
-    { "<leader>oo", function() require("opencode").toggle() end, mode = { "n", "t" }, desc = "Toggle opencode" },
-    { "go", function() return require("opencode").operator("@this ") end, mode = { "n", "x" }, desc = "Add range to opencode", expr = true },
-    { "goo", function() return require("opencode").operator("@this ") .. "_" end, desc = "Add line to opencode", expr = true },
+    {
+      "<leader>oa",
+      function()
+        require("opencode").ask("@this: ", { submit = true })
+      end,
+      mode = { "n", "x" },
+      desc = "Ask opencode",
+    },
+    {
+      "<leader>os",
+      function()
+        require("opencode").select()
+      end,
+      mode = { "n", "x" },
+      desc = "Select opencode action",
+    },
+    {
+      "<leader>oo",
+      function()
+        require("opencode").toggle()
+      end,
+      mode = { "n", "t" },
+      desc = "Toggle opencode",
+    },
+    {
+      "go",
+      function()
+        return require("opencode").operator("@this ")
+      end,
+      mode = { "n", "x" },
+      desc = "Add range to opencode",
+      expr = true,
+    },
+    {
+      "goo",
+      function()
+        return require("opencode").operator("@this ") .. "_"
+      end,
+      desc = "Add line to opencode",
+      expr = true,
+    },
   },
 }
