ed059e23963fc6a2c8758276218bcd9a81fd2570
Niels Mentink <niels@ment.ink>
2026-03-29 03:26:56 +0200

    Update .config/nvim/lua/config/lsp/settings/oxlint.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/oxlint.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/oxlint.lua
@@ -1,9 +1,7 @@
 ---@type vim.lsp.Config
 local oxlint = {
-  root_markers = vim.tbl_extend("force", { "vite.config.ts" }, vim.lsp.config.oxlint.root_markers or {}),
-
   root_dir = function(bufnr, on_dir)
-    local root_markers = { ".oxlintrc.json", "vite.config.ts" }
+    local root_markers = { ".oxlintrc.json", "oxlint.config.ts", "vite.config.ts" }
     root_markers = vim.list_extend(root_markers, { ".git" })
     local project_root = vim.fs.root(bufnr, root_markers)
     on_dir(project_root or vim.fn.getcwd())
