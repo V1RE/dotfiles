66f228a14c0e891b6deca358ac2b7a3f2c05d56b
Niels Mentink <niels@ment.ink>
2026-03-30 10:45:49 +0200

    Revert "Update .config/nvim/lua/config/lsp/handlers.lua"
    This reverts commit 37873ee8647871f4f50a440d33589c11754470d6.

--- dot_config/nvim/exact_lua/config/lsp/handlers.lua
+++ dot_config/nvim/exact_lua/config/lsp/handlers.lua
@@ -1,4 +1,3 @@
-local api = vim.api
 local M = {}
 local i = require("config.icons")
 
@@ -6,107 +5,6 @@ M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.capabilities = require("blink.cmp").get_lsp_capabilities(M.capabilities)
 -- M.capabilities = require("cmp_nvim_lsp").default_capabilities(M.capabilities)
-
-local sig_help_ns = vim.api.nvim_create_namespace("lsp_signature_help")
-
-local function resolve_float_config(config)
-  return vim.tbl_deep_extend("force", config or {}, { border = "rounded" })
-end
-
-local function trim_empty_lines(lines)
-  local start_idx = 1
-  local end_idx = #lines
-
-  while start_idx <= end_idx and lines[start_idx]:match("^%s*$") do
-    start_idx = start_idx + 1
-  end
-
-  while end_idx >= start_idx and lines[end_idx]:match("^%s*$") do
-    end_idx = end_idx - 1
-  end
-
-  if start_idx > end_idx then
-    return {}
-  end
-
-  return vim.list_slice(lines, start_idx, end_idx)
-end
-
-local function as_plaintext_lines(lines)
-  local plain_lines = {} ---@type string[]
-
-  for _, line in ipairs(lines) do
-    if not line:match("^```") then
-      plain_lines[#plain_lines + 1] = line
-    end
-  end
-
-  return trim_empty_lines(plain_lines)
-end
-
-local function hover_handler(_, result, ctx, config)
-  if api.nvim_get_current_buf() ~= ctx.bufnr then
-    return
-  end
-
-  if not (result and result.contents) then
-    return
-  end
-
-  local lines = vim.lsp.util.convert_input_to_markdown_lines(result.contents)
-  lines = as_plaintext_lines(lines)
-
-  if vim.tbl_isempty(lines) then
-    return
-  end
-
-  return vim.lsp.util.open_floating_preview(lines, "text", resolve_float_config(config))
-end
-
-local function signature_help_handler(_, result, ctx, config)
-  config = config or {}
-
-  if api.nvim_get_current_buf() ~= ctx.bufnr then
-    return
-  end
-
-  if not (result and result.signatures and result.signatures[1]) then
-    if config.silent ~= true then
-      print("No signature help available")
-    end
-    return
-  end
-
-  local client = vim.lsp.get_client_by_id(ctx.client_id)
-  local triggers = client
-      and vim.tbl_get(client.server_capabilities, "signatureHelpProvider", "triggerCharacters")
-    or nil
-  local lines, hl = vim.lsp.util.convert_signature_help_to_markdown_lines(result, nil, triggers)
-
-  if not lines then
-    if config.silent ~= true then
-      print("No signature help available")
-    end
-    return
-  end
-
-  lines = as_plaintext_lines(lines)
-
-  if vim.tbl_isempty(lines) then
-    if config.silent ~= true then
-      print("No signature help available")
-    end
-    return
-  end
-
-  local fbuf, fwin = vim.lsp.util.open_floating_preview(lines, "text", resolve_float_config(config))
-
-  if hl then
-    vim.hl.range(fbuf, sig_help_ns, "LspSignatureActiveParameter", { hl[1], hl[2] }, { hl[3], hl[4] })
-  end
-
-  return fbuf, fwin
-end
 
 M.setup = function()
   vim.diagnostic.config({
@@ -142,9 +40,6 @@ M.setup = function()
       prefix = "",
     },
   })
-
-  vim.lsp.handlers["textDocument/hover"] = hover_handler
-  vim.lsp.handlers["textDocument/signatureHelp"] = signature_help_handler
 
   vim.lsp.commands["editor.action.showReferences"] = function(command, ctx)
     local locations = command.arguments[3]
