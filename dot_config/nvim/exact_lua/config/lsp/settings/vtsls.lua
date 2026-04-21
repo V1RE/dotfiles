f581391c6573cb277c61662f725d9ff9fefe258b
Niels Mentink <niels@ment.ink>
2026-04-21 15:42:10 +0200

    Update .config/nvim/lua/config/lsp/settings/vtsls.lua

--- dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
+++ dot_config/nvim/exact_lua/config/lsp/settings/vtsls.lua
@@ -9,26 +9,26 @@ local vtsls = {
   },
 
   -- Only keep code actions; tsgo handles everything else
-  on_attach = function(client)
-    client.server_capabilities.completionProvider = nil
-    client.server_capabilities.hoverProvider = false
-    client.server_capabilities.definitionProvider = false
-    client.server_capabilities.referencesProvider = false
-    client.server_capabilities.renameProvider = false
-    client.server_capabilities.signatureHelpProvider = nil
-    client.server_capabilities.implementationProvider = false
-    client.server_capabilities.typeDefinitionProvider = false
-    client.server_capabilities.documentHighlightProvider = false
-    client.server_capabilities.documentSymbolProvider = false
-    client.server_capabilities.workspaceSymbolProvider = false
-    client.server_capabilities.declarationProvider = false
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-    client.server_capabilities.inlayHintProvider = nil
-    client.server_capabilities.codeLensProvider = nil
-    client.server_capabilities.diagnosticProvider = nil
-    client.server_capabilities.semanticTokensProvider = nil
-  end,
+  -- on_attach = function(client)
+  --   client.server_capabilities.completionProvider = nil
+  --   client.server_capabilities.hoverProvider = false
+  --   client.server_capabilities.definitionProvider = false
+  --   client.server_capabilities.referencesProvider = false
+  --   client.server_capabilities.renameProvider = false
+  --   client.server_capabilities.signatureHelpProvider = nil
+  --   client.server_capabilities.implementationProvider = false
+  --   client.server_capabilities.typeDefinitionProvider = false
+  --   client.server_capabilities.documentHighlightProvider = false
+  --   client.server_capabilities.documentSymbolProvider = false
+  --   client.server_capabilities.workspaceSymbolProvider = false
+  --   client.server_capabilities.declarationProvider = false
+  --   client.server_capabilities.documentFormattingProvider = false
+  --   client.server_capabilities.documentRangeFormattingProvider = false
+  --   client.server_capabilities.inlayHintProvider = nil
+  --   client.server_capabilities.codeLensProvider = nil
+  --   client.server_capabilities.diagnosticProvider = nil
+  --   client.server_capabilities.semanticTokensProvider = nil
+  -- end,
 
   ---@type lspconfig.settings.vtsls
   settings = {
