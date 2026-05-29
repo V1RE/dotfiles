d9a567b9700b500a358cc986fcd2f8c512aff980
Niels Mentink <niels@ment.ink>
2026-05-29 13:59:54 +0200

    Update .config/nvim/lua/core/plugins/treesitter.lua

--- dot_config/nvim/exact_lua/core/exact_plugins/treesitter.lua
+++ dot_config/nvim/exact_lua/core/exact_plugins/treesitter.lua
@@ -1,64 +1,47 @@
+local treesitter_filetypes = {
+  "bash",
+  "css",
+  "csv",
+  "diff",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "graphql",
+  "html",
+  "javascript",
+  "jsdoc",
+  "json",
+  "jsonc",
+  "lua",
+  "luadoc",
+  "markdown",
+  "markdown_inline",
+  "query",
+  "regex",
+  "ruby",
+  "scss",
+  "sql",
+  "svelte",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "xml",
+  "yaml",
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = treesitter_filetypes,
+  callback = function(event)
+    pcall(vim.treesitter.start, event.buf, event.match)
+  end,
+})
+
 ---@type LazyPluginSpec[]
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    main = "nvim-treesitter.configs",
-
-    ---@type TSConfig
-    opts = {
-      modules = {},
-      auto_install = true,
-      ensure_installed = {
-        "bash",
-        "css",
-        "csv",
-        "diff",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "graphql",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "regex",
-        "ruby",
-        "scss",
-        "sql",
-        "svelte",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-      sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-      ignore_install = { "haskell", "elixir", "phpdoc" }, -- List of parsers to ignore installing
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<Enter>",
-          node_incremental = "<Enter>",
-          node_decremental = "<BS>",
-        },
-      },
-    },
-  },
-
   {
     "windwp/nvim-ts-autotag",
     event = "InsertEnter",
